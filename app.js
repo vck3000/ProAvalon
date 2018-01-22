fe3d66b865e3cc0df8ade1bef3ebce25a2197469
Victor Kuo <vkuo3446@uni.sydney.edu.au>
2018-01-22 22:26:57 +1100

    Added mongoose and Partial routes

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
+//=====================================
+//INITIALISATION
+//=====================================
 var express 		= require("express"),
 	app 			= express();
 
 var port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
-
 app.use(express.static("assets"));
 
 
+//=====================================
+//ROUTES
+//=====================================
+
+//Index route
 app.get("/", function(req, res){
 	res.render("index");
 });
 
+//register route
+app.get("/register", function(req, res){
+	res.render("register");
+});
+
+//Post of the register route
+app.post("/", function(req, res){
+
+});
+
+//start server listening
 var server = app.listen(port, function(){
 	console.log("Server has started!");
 });
+
+//=====================================
+//SOCKETS
+//=====================================
