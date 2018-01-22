d8cc88681c671600279b89a8a97eafc1805176e4
Victor Kuo <vkuo3446@uni.sydney.edu.au>
2018-01-22 23:15:59 +1100

    login working

--- app.js
+++ app.js
@@ -5,9 +5,29 @@ var express 		= require("express"),
 	app 			= express()
 	mongoose		= require("mongoose"),
 	bodyParser 		= require("body-parser"),
-	methodOverride 	= require("method-override");
+	methodOverride 	= require("method-override"),
+
+	User 			= require("./models/user"),
+
+	passport		= require("passport"),
+	LocalStrategy	= require("passport-local");
 
 var port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost/TheNewResistanceUsers");
+
+
+//authentication
+app.use(require("express-session")({
+	secret: "Once again Rusty wins cutest dog!",
+	resave: false,
+	saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
@@ -15,7 +35,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 
-mongoose.connect("mongodb://localhost/TheNewResistanceUsers");
 
 
 
@@ -38,7 +57,34 @@ app.get("/register", function(req, res){
 
 //Post of the register route
 app.post("/", function(req, res){
-	res.send("Post req of the register!");
+	var newUser = new User({username: req.body.username});
+	User.register(newUser, req.body.password, function(err, user){
+		if(err){
+			console.log("ERROR: " + err);
+			res.redirect("register");
+		} else{
+			passport.authenticate("local")(req, res, function(){
+				res.redirect("/lobby");
+			});
+		}
+	});
+});
+
+app.post("/login", passport.authenticate("local", {
+	successRedirect: "/lobby",
+	failureRedirect: "/"
+}), function(req, res){
+	res.send("LOGIN LOGIC");
+});
+
+// app.post("/login", function(req, res){
+// 	res.send("post of login");
+// });
+
+
+ 
+app.get("/lobby", isLoggedIn, function(req, res){
+	res.render("lobby");
 });
 
 //start server listening
@@ -49,3 +95,16 @@ var server = app.listen(port, function(){
 //=====================================
 //SOCKETS
 //=====================================
+
+
+
+//=====================================
+//MIDDLEWARE
+//=====================================
+function isLoggedIn(req, res, next){
+	if(req.isAuthenticated()){
+		return next();
+	}
+	console.log("User is not logged in");
+	res.redirect("/");
+}
