d0058fbae2d73ffbef9312217cb3033bb107b052
Victor Kuo <vkuo3446@uni.sydney.edu.au>
2018-07-07 14:46:58 +1000

    Added: Server text to state what your alliance is and what your role is at the start of a game.

--- assets/scripts/lobby.js
+++ assets/scripts/lobby.js
@@ -775,6 +775,16 @@ socket.on("game-data", function (data) {
         // console.log(data);
         gameData = data;
 
+        //if the game has only just started for the first time, display your role
+        if(gameStarted === false){
+            var str = "You are a " + gameData.alliance + ". Your role is " + gameData.role + ".";
+            data = {
+                classStr: "server-text",
+                message: str
+            }
+            addToRoomChat(data);
+        }
+
         gameStarted = true;
 
         //hide the options cog
