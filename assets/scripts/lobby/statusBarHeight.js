232542077b07f220a22e5fb45133cb8006cb8739
Victor Kuo <vkuo3446@uni.sydney.edu.au>
2018-07-22 00:02:39 +1000

    Revert "Notme fix 3"
    This reverts commit 2abaf0cf0bbf6608cec85608545d261ce910994a.

--- assets/scripts/lobby/statusBarHeight.js
+++ assets/scripts/lobby/statusBarHeight.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 });
 //if the users screen is big enough, then we can make the center status bar big
 function checkStatusBarWithHeight(){
-    const cutOffHeight = 750
+    const cutOffHeight = 850
     if($(window).height() > cutOffHeight){
         $("#status").removeClass("well-sm");
     }
