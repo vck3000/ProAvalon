1520b11011c038626647ff4f38372098c5b0ffc3
Victor Kuo <vck3000@gmail.com>
2018-12-03 18:11:05 +1100

    Start of anon mode

---
+++ gameplay/anon.js
@@ -0,0 +1,46 @@
+myObj = {
+    bots: [
+        "Bot1",
+        "Bot2",
+        "Bot3",
+        "Bot4",
+        "Bot5",
+        "Bot6",
+        "Bot7",
+        "Bot8",
+        "Bot9",
+        "Bot10",
+    ],
+
+    pokemon: [
+        "Charmander",
+        "Bulbasaur",
+        "",
+        "Bot4",
+        "Bot5",
+        "Bot6",
+        "Bot7",
+        "Bot8",
+        "Bot9",
+        "Bot10",
+    ],
+
+    animeCharacters: [
+        "Lelouch",
+        "Suzaku",
+        "Naruto",
+        "Sasuke",
+        "Okabe",
+        "Makise",
+        "Luffy",
+        "Zoro",
+        "Yagami",
+        "L",
+    ],
+
+
+}
+
+
+
+module.exports = myObj;
