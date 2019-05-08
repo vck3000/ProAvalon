9571835071e0a176a04e5b9859d3cee9c757bb6d
bocaben <bocaben@gmail.com>
2019-05-08 16:18:08 +0300

    added a new command /gm that is a shortcut for /guessmerlin

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -545,6 +545,13 @@ var actionsObj = {
 				
 				return { message: messageToClient, classStr: "server-text noselect" };
 			}
+		},
+		gm: {
+			command: "gm",
+			help: "/gm <playername>: Shortcut for /guessmerlin",
+			run: function (data, senderSocket) {
+				return actionsObj.userCommands.guessmerlin.run(data, senderSocket);
+			}
 		}
 	},
 
