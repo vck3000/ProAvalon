bd80bc64e453e405365efec01c3fc25ee3aeda21
Alexander Cai <alexandercai@outlook.com>
2019-07-09 11:50:57 -0600

    deleted a file I used for testing

--- gameplay/avalonBot/index.js
+++
@@ -1,8 +0,0 @@
-const { join } = require("path");
-// import { readdirSync } from "fs";
-
-const path = join(__dirname, "cards");
-
-readdirSync(path).forEach((fileName) => {
-    if (fileName.endsWith(".js")) module.exports[fileName] = require(`./cards/${fileName}`);
-});
