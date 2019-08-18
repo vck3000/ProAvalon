b4f87a6fa7345347f7ef8030199e4ceefc732a70
Victor Kuo <vck3000@gmail.com>
2019-08-18 22:59:54 +1000

    Fixed mods not showing up correctly

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -316,7 +316,7 @@ var actionsObj = {
             command: 'mods',
             help: '/mods: Shows a list of online moderators.',
             run() {
-                const modUsers = getPlayerUsernamesFromAllSockets().filter((username) => modsArray.includes(username));
+                const modUsers = getPlayerUsernamesFromAllSockets().filter((username) => modsArray.includes(username.toLowerCase()));
                 const message = `Currently online mods: ${modUsers.length > 0 ? modUsers.join(', ') : 'None'}.`;
                 return { message, classStr: "server-text" };
             }
