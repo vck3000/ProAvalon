94536ef920aeaa894ea3a02870899b4faa249106
Victor Kuo <vck3000@gmail.com>
2019-08-18 23:02:59 +1000

    Fixed couldn't send message to mod. Also fixed please wait x seconds prompt.

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -331,7 +331,7 @@ var actionsObj = {
                 const lastPmTime = pmmodCooldowns[senderSocket.id];
                 if (lastPmTime) {
                     const remaining = new Date() - lastPmTime;
-                    if (remaining < PMMOD_TIMEOUT) return { message: `Please wait ${Math.ceil((PMMOD_TIMEOUT - remaining) / 1000)} seconds before sending another pm!` };
+                    if (remaining < PMMOD_TIMEOUT) return { message: `Please wait ${Math.ceil((PMMOD_TIMEOUT - remaining) / 1000)} seconds before sending another pm!`, classStr: 'server-text' };
                 }
                 // Checks for various missing fields or errors
                 if (!args[1]) return { message: 'Please specify a mod to message. Type /mods to get a list of online mods.', classStr: 'server-text' };
@@ -339,7 +339,7 @@ var actionsObj = {
                 const modSocket = allSockets[getIndexFromUsername(allSockets, args[1], true)];
                 if (!modSocket) return senderSocket.emit('messageCommandReturnStr', { message: `Could not find ${args[1]}`, classStr: "server-text" });
                 if (modSocket.id === senderSocket.id) return { message: 'You cannot private message yourself!', classStr: "server-text" };
-                if (!modsArray.includes(args[1])) return { message: `${args[1]} is not a mod. You may not private message them.`, classStr: 'server-text' };
+                if (!modsArray.includes(args[1].toLowerCase())) return { message: `${args[1]} is not a mod. You may not private message them.`, classStr: 'server-text' };
 
                 let str = `${senderSocket.request.user.username}->${args[1]} (pmmod): ${args.slice(2).join(' ')}`;
 
@@ -353,10 +353,7 @@ var actionsObj = {
                 senderSocket.emit('roomChatToClient', dataMessage);
 
                 modSocket.emit('allChatToClient', dataMessage);
-                modSocket.emit('roomChatToClient', dataMessage)
-
-                lastWhisperObj[modSocket.request.user.username] = senderSocket.request.user.username;
-                lastWhisperObj[senderSocket.request.user.username] = modSocket.request.user.username;
+                modSocket.emit('roomChatToClient', dataMessage);
 
                 // Set a cooldown for the sender until they can send another pm
                 pmmodCooldowns[senderSocket.id] = new Date();
