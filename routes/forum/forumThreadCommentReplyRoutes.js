0c2177efb6db4cea0d4f96c776df30adedd4886c
Victor Kuo <vck3000@gmail.com>
2019-09-22 15:14:29 +1000

    Catch an error

--- routes/forum/forumThreadCommentReplyRoutes.js
+++ routes/forum/forumThreadCommentReplyRoutes.js
@@ -132,7 +132,10 @@ async function createReply(req, res, commentReplyData, replyingToThisReply) {
     // since there is a new comment, the thread is now refreshed and no one has seen the new
     // changes yet, except for the one who made the comment
     foundForum.seenUsers = [req.user.username.toLowerCase()];
-    foundForum.save();
+    foundForum.markModified("seenUsers");
+    foundForum.save().catch(err => {
+        console.log(err);
+    });
 
     // redirect to same forum thread
     res.redirect(`/forum/show/${req.params.id}`);
