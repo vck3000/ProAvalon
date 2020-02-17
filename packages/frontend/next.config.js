7b456fc763a730e51a8394a994c0cd8084e0fbf7
Victor Kuo <vck3000@gmail.com>
2020-02-18 00:11:52 +1100

    Finished most of index page front end visuals - Removed bootstrap and FontAwesome - Added with semantic-ui-react - Added Pam's design of the index page - Added fonts, resources, css to work in the project

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -9,6 +9,16 @@ module.exports = withCSS({
         exclude: /node_modules/,
         loader: 'eslint-loader',
       });
+      config.module.rules.push({
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 100000,
+            name: '[name].[ext]',
+          },
+        },
+      });
     }
     return config;
   },
