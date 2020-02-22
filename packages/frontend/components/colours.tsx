02ef5228d2034f9928c0b6d7b1edba7cb85019b7
Victor Kuo <vck3000@gmail.com>
2020-02-22 16:45:15 +1100

    Added desktop navbar. - Added a colours.tsx file. - Mobile view is not complete yet

---
+++ packages/frontend/components/colours.tsx
@@ -0,0 +1,2 @@
+export const GOLD = '#a37d18';
+export const GOLD_HOVER = '#8a6d20';
--- packages/frontend/components/nav.tsx
+++ packages/frontend/components/nav.tsx
@@ -1,52 +1,105 @@
 import React, { ReactElement } from 'react';
 import Link from 'next/link';
-
-const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map((link) => ({
-  ...link,
-  key: `nav-link-${link.href}-${link.label}`,
-}));
+import { GOLD } from './colours';
 
 const Nav = (): ReactElement => (
   <nav>
-    <ul>
-      <li>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-      </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
-          <a href={href}>{label}</a>
+    <div className="nav_div">
+      <span className="before_nav_div" />
+      <ul>
+        <li>
+          <Link href="/lobby">
+            <a>Lobby</a>
+          </Link>
         </li>
-      ))}
-    </ul>
+        <li>
+          <Link href="/rules">
+            <a>Rules</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Community</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/stats">
+            <a>Stats</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/">
+            <a>Development</a>
+          </Link>
+        </li>
+      </ul>
+      <span className="after_nav_div" />
+    </div>
 
     <style jsx>
       {`
         nav {
-          text-align: center;
           width: 100%;
           position: fixed;
         }
-        ul {
+        nav > .nav_div {
+          border-top: 2px solid ${GOLD};
+          border-bottom: 2px solid ${GOLD};
+          position: relative;
+          width: 80%;
+          padding: 6px 50px;
+          margin: 20px auto 0px auto;
+        }
+        nav > .nav_div > ul {
+          padding: 0;
+          margin: 0;
           display: flex;
           justify-content: space-between;
         }
-        nav > ul {
-          padding: 4px 0px;
-          margin: 0px 0px;
+        // This is for the triangle border on the right side
+        nav > .nav_div > .after_nav_div {
+          position: absolute;
+          right: 0%;
+          top: -2px;
+          height: 48px;
+          width: 48px;
+          background-color: ${GOLD};
+          clip-path: polygon(
+            100% 0%,
+            28% 50%,
+            100% 100%,
+            108% 100%,
+            35% 50%,
+            108% 0%
+          );
+        }
+        // Left side is identical to right side except for a vertical flip.
+        nav > .nav_div > .before_nav_div {
+          position: absolute;
+          left: 0%;
+          top: -2px;
+          height: 48px;
+          width: 48px;
+          background-color: ${GOLD};
+          clip-path: polygon(
+            100% 0%,
+            28% 50%,
+            100% 100%,
+            108% 100%,
+            35% 50%,
+            108% 0%
+          );
+          scale: -1;
         }
         li {
           display: flex;
           padding: 6px 8px;
         }
         a {
-          color: #067df7;
+          color: ${GOLD};
           text-decoration: none;
-          font-size: 13px;
+          font-size: 16px;
+          font-family: Montserrat-Bold;
         }
       `}
     </style>
