a0b021e567c322fd6dd7bba7c34240fc26ac5abe
esme <23175119+esme@users.noreply.github.com>
2020-03-29 00:12:52 -0700

    Fixed messages

--- packages/frontend/store/chat/actions.ts
+++ packages/frontend/store/chat/actions.ts
@@ -3,20 +3,9 @@ import {
   ISetConnected,
   SET_CONNECTED,
   IMessage,
-  SET_MESSAGE,
   RECEIVED_MESSAGE,
-  ISetMessageAction,
   IReceivedMessageAction,
 } from './types';
-
-export const setMessage = (
-  messageText: IMessage['messageText'],
-): ISetMessageAction => {
-  return {
-    type: SET_MESSAGE,
-    messageText,
-  };
-};
 
 export const receivedMessage = (message: IMessage): IReceivedMessageAction => {
   return {
