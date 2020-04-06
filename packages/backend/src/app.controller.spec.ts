08c263ca3b63182cf5a1b58620dc17ab6ce9aae3
esme <23175119+esme@users.noreply.github.com>
2020-04-05 23:26:50 -0700

    Implemented basic auth functionality on server - Added passport local and jwt strategies - Added secret key env variable - Added bcrypt for hashing password - Mocked users for now

--- packages/backend/src/app.controller.spec.ts
+++ packages/backend/src/app.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [ChatModule, AuthModule, UsersModule],
       controllers: [AppController],
       providers: [AppService],
     }).compile();
---
+++ packages/backend/src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && this.usersService.comparePassword(pass, user.password)) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+
+  async signup(user: any) {
+    const { username, password } = user;
+    this.usersService.save({ username, password });
+  }
+}
