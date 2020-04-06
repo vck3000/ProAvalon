d930a788ca927690338283d4c8be29783defee27
esme <23175119+esme@users.noreply.github.com>
2020-04-05 17:17:45 -0700

    changed socket io reconnection config

--- packages/frontend/socket/socket.ts
+++ packages/frontend/socket/socket.ts
@@ -4,6 +4,9 @@ import { getApiUrl, getEnv } from '../config';
 
 const socket = io(getApiUrl(), {
   reconnection: true,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  reconnectionAttempts: 5,
 });
 
 // Don't connect if we are the server! There should be a better way to do this.
