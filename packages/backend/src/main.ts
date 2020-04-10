6881d5fa247c2da8f44f5e40c3c121914971b533
esme <23175119+esme@users.noreply.github.com>
2020-04-10 14:35:39 -0700

    Added express rate limiter and helmet for additional security Installed class transformer for pipe validator package Added test for empty username

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import * as rateLimit from 'express-rate-limit';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
   await app.listen(3001);
 }
 bootstrap();
