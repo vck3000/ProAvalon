67895be1d3ca444cfd122fe86b542761dd8db352
Victor Kuo <vck3000@gmail.com>
2020-04-15 22:49:05 +1000

    Renamed players to onlinePlayers. - Also removed initial online players

--- packages/frontend/socket/players.ts
+++ packages/frontend/socket/players.ts
@@ -1,16 +1,16 @@
 import SocketEvents from '../proto/socketEvents';
 import { store } from '../store';
-import { setPlayers } from '../store/players/actions';
-import { IPlayer } from '../store/players/actions.types';
+import { setOnlinePlayers } from '../store/onlinePlayers/actions';
+import { IOnlinePlayer } from '../store/onlinePlayers/actions.types';
 
 export const SetSocketPlayersEvents = (socket: SocketIOClient.Socket): void => {
   socket.on(
     SocketEvents.ONLINE_USERS_TO_CLIENT,
-    (onlineUsers: IPlayer['username'][]) => {
+    (onlineUsers: IOnlinePlayer['username'][]) => {
       const players = onlineUsers.map((username) => ({
         username,
       }));
-      store.dispatch(setPlayers(players));
+      store.dispatch(setOnlinePlayers(players));
     },
   );
 };
