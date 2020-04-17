ad10aebbaa58f7af7d21d2bed1283168834ad5f0
esme <23175119+esme@users.noreply.github.com>
2020-04-17 16:59:30 -0700

    Added chatResponse and onlinePlayers validation using class-transformer-validator on the frontend

--- packages/frontend/socket/onlinePlayers.ts
+++ packages/frontend/socket/onlinePlayers.ts
@@ -1,10 +1,18 @@
-import { SocketEvents } from '../proto/lobbyProto';
+import { transformAndValidate } from 'class-transformer-validator';
+import { SocketEvents, OnlinePlayer } from '../proto/lobbyProto';
 import { store } from '../store';
 import { setOnlinePlayers } from '../store/onlinePlayers/actions';
-import { IOnlinePlayer } from '../store/onlinePlayers/actions.types';
 
 export const SetSocketPlayersEvents = (socket: SocketIOClient.Socket): void => {
-  socket.on(SocketEvents.ONLINE_PLAYERS, (players: IOnlinePlayer[]) => {
-    store.dispatch(setOnlinePlayers(players));
-  });
+  socket.on(
+    SocketEvents.ONLINE_PLAYERS,
+    async (onlinePlayers: OnlinePlayer[]) => {
+      try {
+        const players = await transformAndValidate(OnlinePlayer, onlinePlayers);
+        store.dispatch(setOnlinePlayers(players));
+      } catch (err) {
+        throw Error(`Validation failed. Error: ${err}`);
+      }
+    },
+  );
 };
