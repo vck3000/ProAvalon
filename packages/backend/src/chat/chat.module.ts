73fbc06d70288405065992c8c610e01cdf5e2bfe
esme <23175119+esme@users.noreply.github.com>
2020-04-20 16:37:17 -0700

    Removed redis adapter from providers and renamed getCommand to sendCommand

--- packages/backend/src/chat/chat.module.ts
+++ packages/backend/src/chat/chat.module.ts
@@ -3,11 +3,10 @@ import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
 import { UserCommandsService } from '../chat-commands/user-commands/user-commands.service';
-import RedisAdapter from '../util/redisAdapter';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, ChatGateway, UserCommandsService, RedisAdapter],
+  providers: [ChatService, ChatGateway, UserCommandsService],
   exports: [ChatService],
 })
 export class ChatModule {}
