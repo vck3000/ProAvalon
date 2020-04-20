eb6313d605916b4eddf4f54da7ed8d4c6498c3a1
esme <23175119+esme@users.noreply.github.com>
2020-04-20 16:12:59 -0700

    Modularized chat commands to have a user commands subfolder Standardized sendCommand to take in command, data and socket WIP Getting server from Redis Adapter

---
+++ packages/backend/src/chat-commands/user-commands/actions/userInteractions.ts
@@ -0,0 +1,38 @@
+import { SocketUser } from '../../../users/users.socket';
+import { emitChatResponse } from '../../chatResponse';
+import { UserCommand } from '../interfaces/user-commands.interface';
+
+export class UserInteraction implements UserCommand {
+  command: string;
+
+  pastTenseCommand: string;
+
+  help: string;
+
+  constructor(command: string, pastTenseCommand: string) {
+    this.command = command;
+    this.pastTenseCommand = pastTenseCommand;
+    this.help = `/${command} <playername>: ${command} a player.`;
+  }
+
+  run(data: string[], senderSocket: SocketUser) {
+    emitChatResponse(
+      `${senderSocket.user.displayUsername} has ${this.pastTenseCommand} you!`,
+      // change later
+      senderSocket,
+    );
+    emitChatResponse(
+      `You have ${this.pastTenseCommand} ${data[0]}!`,
+      senderSocket,
+    );
+  }
+}
+
+export const userInteractions: Record<string, any> = {
+  buzz: new UserInteraction('buzz', 'buzzed'),
+  lick: new UserInteraction('lick', 'licked'),
+  slap: new UserInteraction('slap', 'slapped'),
+  punch: new UserInteraction('punch', 'punched'),
+  boop: new UserInteraction('boop', 'booped'),
+  tickle: new UserInteraction('tickle', 'tickled'),
+};
--- packages/backend/src/chat/chat.module.ts
+++ packages/backend/src/chat/chat.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ChatController } from './chat.controller';
 import { ChatGateway } from './chat.gateway';
 import { ChatService } from './chat.service';
-import { UserCommandsService } from './user-commands/user-commands.service';
+import { UserCommandsService } from '../chat-commands/user-commands/user-commands.service';
+import RedisAdapter from '../util/redisAdapter';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, ChatGateway, UserCommandsService],
+  providers: [ChatService, ChatGateway, UserCommandsService, RedisAdapter],
   exports: [ChatService],
 })
 export class ChatModule {}
