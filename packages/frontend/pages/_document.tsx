1d551d436d781e5aa8d4aac965a3799f14014f98
skipkayhil <skipkayhil@gmail.com>
2020-05-08 01:51:36 -0400

    make night theme default for SSR

--- packages/frontend/pages/_document.tsx
+++ packages/frontend/pages/_document.tsx
@@ -18,7 +18,7 @@ class MyDocument extends Document {
             href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
           />
         </Head>
-        <body>
+        <body className="night">
           <Main />
           <NextScript />
         </body>
