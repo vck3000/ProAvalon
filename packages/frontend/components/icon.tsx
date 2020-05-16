fc58cae5b833cc4cabc3f0924eee7c51aaa14858
skipkayhil <skipkayhil@gmail.com>
2020-05-16 17:37:27 -0400

    Add game state reducer and vote history component

--- packages/frontend/components/icon.tsx
+++ packages/frontend/components/icon.tsx
@@ -18,8 +18,14 @@ const Envelope = (): ReactElement => (
   </svg>
 );
 
+const Check = (): ReactElement => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />
+  </svg>
+);
+
 type Props = {
-  name: 'user' | 'lock' | 'envelope';
+  name: 'user' | 'lock' | 'envelope' | 'check';
 };
 
 const Icon = ({ name }: Props): ReactElement => {
@@ -30,6 +36,8 @@ const Icon = ({ name }: Props): ReactElement => {
       return <Lock />;
     case 'envelope':
       return <Envelope />;
+    case 'check':
+      return <Check />;
     default:
       return <></>;
   }
