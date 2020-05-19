b7a5bcc2e03017f050da77a146b21c86b000c3bd
Victor Kuo <vck3000@gmail.com>
2020-05-19 15:40:50 +1000

    Fixed rest of @proavalon/proto implementation. - Circle CI runs correctly. - Dto's have all been moved into @proavalon/proto. - Set up testing for @proavalon/proto. - Moved backend integration test to root ProAvalon folder.

--- packages/backend/src/forums/forums.controller.ts
+++ packages/backend/src/forums/forums.controller.ts
@@ -1,16 +1,13 @@
 import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { CreateForumPostDto, CreateForumCommentDto } from '@proavalon/proto';
 import { ForumsService } from './forums.service';
-import { CreateForumPostDto } from './dto/create-forumpost.dto';
-import { CreateForumCommentDto } from './dto/create-forumcomment.dto';
 
 @Controller('forums')
 export class ForumsController {
   constructor(private readonly forumsService: ForumsService) {}
 
   @Post()
-  async addNewForumPost(
-    @Body() createForumPostDto: CreateForumPostDto,
-  ) {
+  async addNewForumPost(@Body() createForumPostDto: CreateForumPostDto) {
     const postId = await this.forumsService.addPost(createForumPostDto);
     return { id: postId };
   }
@@ -19,7 +16,9 @@ export class ForumsController {
   async addNewForumComment(
     @Body() createForumCommentDto: CreateForumCommentDto,
   ) {
-    const commentId = await this.forumsService.addComment(createForumCommentDto);
+    const commentId = await this.forumsService.addComment(
+      createForumCommentDto,
+    );
     return { id: commentId };
   }
 
