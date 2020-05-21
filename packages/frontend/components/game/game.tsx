ef0b48502612691f446445b92b74ba1da5047b83
skipkayhil <skipkayhil@gmail.com>
2020-05-21 00:40:43 -0400

    Add gameContent component, refactor game component
    - remove extra divs by passing classes straight to children

--- packages/frontend/components/game/game.tsx
+++ packages/frontend/components/game/game.tsx
@@ -1,26 +1,44 @@
 import { ReactElement } from 'react';
+import css from 'styled-jsx/css';
 import Layout from '../layout/layout';
 import TabPane from './tabPane';
 import useGame from './useGame';
+import GameContent from './gameContent';
 
 interface IGameProps {
   gameID?: string | string[];
 }
+
+const { styles, className } = css.resolve`
+  .gameContent {
+    flex: 1;
+  }
+
+  .tabContainer {
+    flex: 1;
+    display: flex;
+  }
+
+  .tabPane {
+    flex: 1;
+    padding: 1rem;
+    box-sizing: border-box; /* tabPane has height 100%, need this for padding*/
+  }
+`;
+
+const getClass = (classes: string): string => `${className} ${classes}`;
 
 const GameIndex = ({ gameID }: IGameProps): ReactElement => {
   useGame(gameID);
 
   return (
     <Layout>
-      <div style={{ flex: 1 }}>Game {gameID} Content</div>
-      <div style={{ flex: 1, display: 'flex' }}>
-        <div style={{ flex: 1, padding: '1rem' }}>
-          <TabPane />
-        </div>
-        <div style={{ flex: 1, padding: '1rem' }}>
-          <TabPane />
-        </div>
+      <GameContent className={getClass('gameContent')} game={{}} />
+      <div className={getClass('tabContainer')}>
+        <TabPane className={getClass('tabPane')} />
+        <TabPane className={getClass('tabPane')} />
       </div>
+      {styles}
     </Layout>
   );
 };
