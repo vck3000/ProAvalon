4d0da887c095814b35acf01feb8752f3908f11f8
Victor Kuo <vck3000@gmail.com>
2020-07-05 14:31:31 +1000

    Mod change

--- modsadmins/mods.js
+++ modsadmins/mods.js
@@ -1,11 +1,4 @@
 // all in lower case
-const modsArray = [
-  "pronub",
-  "ref-rain",
-  "muse",
-  "starbird1002",
-  "not.me",
-  "superkushal",
-];
+const modsArray = ['pronub', 'ref-rain', 'muse', 'starbird1002', 'superkushal'];
 
 module.exports = modsArray;
