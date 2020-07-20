9c2fe072f6eb4752f8954004b86aec9c5c1f5ded
Victor Kuo <vck3000@gmail.com>
2020-07-20 20:43:38 +1000

    Mod changes

--- modsadmins/mods.js
+++ modsadmins/mods.js
@@ -1,4 +1,11 @@
 // all in lower case
-const modsArray = ['pronub', 'ref-rain', 'muse', 'starbird1002', 'superkushal'];
+const modsArray = [
+  'pronub',
+  'muse',
+  'starbird1002',
+  'superkushal',
+  'besjbo',
+  'hakha3',
+];
 
 module.exports = modsArray;
