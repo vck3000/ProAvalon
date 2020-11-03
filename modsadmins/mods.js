677cdb04838987111c5c390ea75dbb71d00bc722
Victor Kuo <vck3000@gmail.com>
2020-11-03 18:25:21 +1100

    Remodded not.me

--- modsadmins/mods.js
+++ modsadmins/mods.js
@@ -1,4 +1,4 @@
 // all in lower case
-const modsArray = ['pronub', 'starbird1002', 'besjbo', 'hakha3'];
+const modsArray = ['pronub', 'starbird1002', 'besjbo', 'hakha3', 'not.me'];
 
 module.exports = modsArray;
