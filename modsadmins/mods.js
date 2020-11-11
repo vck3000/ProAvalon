e5197c75e14fa39d79b8d7613ca996cbb313d1ca
Victor Kuo <vck3000@gmail.com>
2020-11-11 11:07:58 +1100

    Unmod hakha

--- modsadmins/mods.js
+++ modsadmins/mods.js
@@ -1,4 +1,4 @@
 // all in lower case
-const modsArray = ['pronub', 'starbird1002', 'besjbo', 'hakha3', 'not.me', 'anders', 'morgenstern'];
+const modsArray = ['pronub', 'starbird1002', 'besjbo', 'not.me', 'anders', 'morgenstern'];
 
 module.exports = modsArray;
