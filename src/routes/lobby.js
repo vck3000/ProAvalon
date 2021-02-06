c376e281664c5b04e9e0e251c549c747ac852451
Victor Kuo <vck3000@gmail.com>
2021-02-07 00:06:09 +1100

    Testing

--- src/routes/lobby.js
+++ src/routes/lobby.js
@@ -2,11 +2,20 @@ const { Router } = require('express');
 const router = new Router();
 
 router.get('/', (req, res) => {
-    console.log('ip:', req.ip);
-    res.render('lobby', {
-        headerActive: 'lobby',
-        optionsCog: true,
-    });
+  console.log('ip:', req.ip);
+
+  const ip =
+    req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    (req.connection.socket ? req.connection.socket.remoteAddress : null);
+
+  console.log('alternate ip:', ip);
+
+  res.render('lobby', {
+    headerActive: 'lobby',
+    optionsCog: true,
+  });
 });
 
 module.exports = router;
