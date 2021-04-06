1b001bd445fd555b9b1d9e51c8c7a01f7c973ee1
Victor Kuo <vck3000@gmail.com>
2021-04-06 11:55:58 +1000

    Added manu

--- src/modsadmins/mods.js
+++ src/modsadmins/mods.js
@@ -1,4 +1,11 @@
 // all in lower case
-const modsArray = ['pronub', 'starbird1002', 'besjbo', 'not.me', 'anders'];
+const modsArray = [
+  'pronub',
+  'starbird1002',
+  'besjbo',
+  'not.me',
+  'anders',
+  'manuavalon',
+];
 
 module.exports = modsArray;
