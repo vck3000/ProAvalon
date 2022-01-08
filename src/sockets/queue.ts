66e71696e6d657efecf329a549e850fd00f4f10e
Ayad Farhat <ayad.farhat2001@icloud.com>
2022-01-08 09:17:26 +0200

    Created: Queue class.

--- src/sockets/queue.ts
+++ src/sockets/queue.ts
@@ -1,15 +1,36 @@
 export class Queue {
   unrankedQueue: string[];
+  rankedQueue: string[];
 
   constructor() {
     this.unrankedQueue = [];
+    this.rankedQueue = [];
   }
 
-  joinQueue(username: string): void {
-    this.unrankedQueue.push(username);
+  joinUnrankedQueue(username: string): void {
+    this.unrankedQueue.push(username.toLowerCase());
+  }
+  joinRankedQueue(username: string): void {
+    this.rankedQueue.push(username.toLowerCase());
+  }
+
+  leaveUnrankedQueue(username: string): void {
+    var index =this.unrankedQueue.indexOf(username.toLowerCase());
+    if(index!= -1){
+      this.unrankedQueue.splice(index);
+    }
+  }
+  leaveRankedQueue(username: string): void {
+    var index =this.rankedQueue.indexOf(username.toLowerCase());
+    if(index!= -1){
+      this.rankedQueue.splice(index);
+    }
   }
 
   getQueueSizeUnranked(): number {
     return this.unrankedQueue.length;
+  }
+  getQueueSizeRanked(): number {
+    return this.rankedQueue.length;
   }
 }
--- src/sockets/tests/queue.test.ts
+++ src/sockets/tests/queue.test.ts
@@ -4,8 +4,21 @@ describe('Queue', () => {
   it('adds a socket to the queue when they join', () => {
     const queue = new Queue();
 
-    queue.joinQueue('pronub');
-
+    queue.joinUnrankedQueue('pronub');
+    queue.joinRankedQueue('pronub');
     expect(queue.getQueueSizeUnranked()).toEqual(1);
+    expect(queue.getQueueSizeRanked()).toEqual(1);
+  });
+
+  it('remove a username from the queue', () => {
+    const queue = new Queue();
+
+    queue.joinUnrankedQueue('pronub');
+    queue.joinRankedQueue('pronub');
+
+    queue.leaveUnrankedQueue('pronub');
+    queue.leaveRankedQueue('pronub');
+    expect(queue.getQueueSizeUnranked()).toEqual(0);
+    expect(queue.getQueueSizeRanked()).toEqual(0);
   });
 });
