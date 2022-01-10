7e337c1e82d64ef2eb449fbeeb36c5bec6ab1adb
Victor Kuo <vck3000@gmail.com>
2022-01-10 17:53:35 +1100

    Added test modal.

--- src/views/components/report/index.tsx
+++ src/views/components/report/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { TestModal } from './modal'
 
 function Report() {
-  return <h1>Hi <button onClick={() => {console.log("HI")}}>CLICK ME </button></h1>;
+  return (
+    <h1>
+      Hi 
+      <button onClick={() => {console.log("HI")}}>CLICK ME</button>
+      <TestModal />
+    </h1>
+  );
 }
 
 export default hot(Report);
---
+++ src/views/components/report/modal.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#reportDiv');
+
+export function TestModal() {
+  const subtitleRef = React.useRef<HTMLHeadingElement>(null);
+
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    // @ts-ignore
+    subtitleRef.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={subtitleRef}>Hello World!</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  );
+}
