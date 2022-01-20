e195642e60edda9b4c495d603e1cbf9d723ee894
Victor Kuo <vck3000@gmail.com>
2022-01-20 18:03:47 +1100

    Updated ExpandableComponent and implemented in report.

--- src/views/components/mod/report/index.tsx
+++ src/views/components/mod/report/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { hot } from 'react-hot-loader/root';
 import Swal from 'sweetalert2';
+import { ExpandableComponent } from '../../common/expandableComponent';
 
 import { myStyle } from './css';
 
@@ -190,77 +191,21 @@ function ReportEntry({ report, callbackOnResolve }: ReportEntryProps) {
         </div>
       )}
       <br />
-      <ViewChat allChat5Mins={report.allChat5Mins} roomChat={report.roomChat} />
+
+      <h4>All Chat (5 mins):</h4>
+      <ExpandableComponent
+        data={report.allChat5Mins.split('\n').map((chat) => (
+          <p style={{ marginBottom: 0 }}>{chat}</p>
+        ))}
+      />
+
+      <h4>Room Chat:</h4>
+      <ExpandableComponent
+        data={report.roomChat.split('\n').map((chat) => (
+          <p style={{ marginBottom: 0 }}>{chat}</p>
+        ))}
+      />
     </div>
-  );
-}
-
-function ChatMessage({ chatMessages }: any) {
-  return (
-    <div style={{ border: 'solid' }}>
-      {chatMessages.split('\n').map((chat: string) => (
-        <li>{chat}</li>
-      ))}
-    </div>
-  );
-}
-
-type ViewChatProps = {
-  allChat5Mins: string;
-  roomChat: string;
-};
-
-function ViewChat({ allChat5Mins, roomChat }: ViewChatProps) {
-  const [collapsedAll, setCollapsedAll] = useState(true);
-  const [collapsedRoom, setCollapsedRoom] = useState(true);
-  return (
-    <span>
-      <div>
-        {collapsedAll ? (
-          <button
-            onClick={() => {
-              setCollapsedAll(false);
-            }}
-          >
-            View All Chat
-          </button>
-        ) : (
-          <div>
-            <button
-              onClick={() => {
-                setCollapsedAll(true);
-              }}
-            >
-              Close All Chat
-            </button>{' '}
-            <ul>{<ChatMessage chatMessages={allChat5Mins} />}</ul>
-          </div>
-        )}
-      </div>
-      <br />
-      <div>
-        {collapsedRoom ? (
-          <button
-            onClick={() => {
-              setCollapsedRoom(false);
-            }}
-          >
-            View Room Chat
-          </button>
-        ) : (
-          <div>
-            <button
-              onClick={() => {
-                setCollapsedRoom(true);
-              }}
-            >
-              Close Room Chat
-            </button>{' '}
-            <ul>{<ChatMessage chatMessages={roomChat} />}</ul>
-          </div>
-        )}
-      </div>
-    </span>
   );
 }
 
