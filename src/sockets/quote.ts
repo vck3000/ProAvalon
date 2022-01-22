4d5b4d0ee96f6e5921029621f541dc7977b559d8
Cindy Chen <cindy.yuanchen11@gmail.com>
2022-01-22 14:05:22 +1100

    Quote Refactoring Process.

--- src/sockets/quote.ts
+++ src/sockets/quote.ts
@@ -1,25 +1,53 @@
+import crypto from 'crypto';
+// @ts-ignore
+import BinarySearchTree from 'binary-search-tree';
+const AVLTree = BinarySearchTree.AVLTree;
+
+// AVLTree = require('binary-search-tree').AVLTree
+
+// create type Message with elements timestamp, username, and message
 export type Message = {
   timestamp: Date;
   username: string;
   message: string;
 };
 
+// create Quote class 
 export class Quote {
-  pastMessages: Message[];
+  // pastMessages: Message[]; // actual hash table
+  pastMessages: any; // Binary search tree
 
+  // Initialise the pastMesssages as an empty array 
   constructor() {
-    this.pastMessages = [];
+    this.pastMessages = new AVLTree();
   }
 
-  isQuote(message: Message): boolean {
-    return this.pastMessages.indexOf(message) !== -1;
+  // String to Hash converter
+  hash = (input: Message): string => {
+    const messageString = `${input.message}${input.username}${input.timestamp}`;
+    return crypto.createHash('sha256').update(messageString).digest('base64');
+  };
+
+  // 1. Add message to the pastMessages array
+  addMessage(message: Message, type: any) {
+    const messageHash = this.hash(message);
+    this.pastMessages.insert(messageHash, true);
   }
 
-  addMessage(message: Message) {
-    this.pastMessages.push(message);
+  // 2. See if the message is a quote  
+  isQuote(message: Message, type: any): boolean {
+    const messageHash = this.hash(message);
+    return this.pastMessages.search(messageHash).length > 0;
   }
 
+  // 3. Delete the room message 
   deleteRoomMessages(roomId: number) {
-    return;
+    // if message is a quote from a different room, delete
+
+    // const messageHash = this.hash(message);
+    // if (message.isQuote) {
+      // if roomId
+    // }
+    return this.pastMessages.delete(roomId)
   }
 }
--- src/sockets/tests/quote.test.ts
+++ src/sockets/tests/quote.test.ts
@@ -12,8 +12,9 @@ describe('Quote', () => {
     };
 
     // Assertion
-    expect(quote.isQuote(message)).toEqual(false);
+    expect(quote.isQuote(message, 'allchat')).toEqual(false);
   });
+
 
   it('should say a message is a quote for a previously seen message.', () => {
     // Setup
@@ -26,15 +27,41 @@ describe('Quote', () => {
     };
 
     // Trigger
-    quote.addMessage(message);
+    quote.addMessage(message, 'allchat');
 
     const message2: Message = {
+      message: 'Hello!',
+      username: 'cin333',
+      timestamp: message.timestamp
+    };
+
+    // Assertion
+    expect(quote.isQuote(message2, 'allchat')).toEqual(true);
+
+    // const message3: Message = {
+    //   message: 'hello!',
+    //   username: 'cin333',
+    //   timestamp: message.timestamp
+    // };
+
+    // // Assertion
+    // expect(quote.isQuote(message3)).toEqual(false);
+  });
+
+  it('should say a message is not a quote for a previously seen message in a different room.', () => {
+    // Setup
+    const quote = new Quote();
+
+    const message: Message = {
       message: 'Hello!',
       username: 'cin333',
       timestamp: new Date(),
     };
 
+    // Trigger
+    quote.addMessage(message, 'allchat');
+    
     // Assertion
-    expect(quote.isQuote(message2)).toEqual(true);
+    expect(quote.isQuote(message, 0)).toEqual(false);
   });
 });
