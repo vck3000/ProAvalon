c001719b13249fbf4faa21486612ec50d4f3b650
Vinsensius Kelvin Wang <62001746+VKelvinWang@users.noreply.github.com>
2023-04-05 20:45:24 +1000

    Update Spy.js

--- src/gameplayv2/resistance/entities/Spy.js
+++ src/gameplayv2/resistance/entities/Spy.js
@@ -1,25 +1,28 @@
 import { alliance } from  '../components/alliance'
 import { world } from '../system'
 
-// Resistance.nextId = 0;
-// class Spy extends Player{
-//     constructor(){
-//         this.id = Entity.getNextID();
-//         this.components = [];
-//     }
+Resistance.nextId = 0;
+class Spy extends Player{
+    constructor(){
+        this.id = Entity.getNextID();
+        this.components = [];
+        this.addComponent(new AllianceComponent("Spy"));
+        this.addComponent(new RoleComponent("Spy"));
+        this.addComponent(new DescriptionComponent("A standard Spy member."));
+    }
 
-//     addComponent(component){
-//         this.components.push(component);
-//     }
+    addComponent(component){
+        this.components.push(component);
+    }
 
-//     removeComponent(component) {
-//         const index = this.components.indexOf(component);
-//         if (index !== -1) {
-//           this.components.splice(index, 1);
-//         }
-//     }
+    removeComponent(component) {
+        const index = this.components.indexOf(component);
+        if (index !== -1) {
+          this.components.splice(index, 1);
+        }
+    }
 
-//     static getNextID() {
-//         return Spy.nextId++;
-//     }
-// }
+    static getNextID() {
+        return Spy.nextId++;
+    }
+}
