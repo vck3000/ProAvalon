ba48d30ca3fbaf51248c9d8b0d19c60c0f7688cf
songd233 <songdino@foxmail.com>
2023-04-16 20:33:30 +1000

    more leader tags

---
+++ src/gameplayv2/resistance/components/leader.h.js
@@ -0,0 +1,10 @@
+import { Component } from './components/RoleComponent';
+
+// a simple tag component
+class leader {
+    constructor () {
+        // this.entityID = entityID
+        this.name = "Leader"
+        this.isLearder = true
+    }
+}
--- src/gameplayv2/resistance/components/leader.h.ts
+++
@@ -1,3 +0,0 @@
-export interface Leader{
-    isLeader?: boolean;
-}
