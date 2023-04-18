785edb6f4abf4e21845e628e4179273299ab7e85
Ming25 <mingxie25@gmail.com>
2023-04-18 22:36:19 +1000

    fix: update route name

--- src/routes/match.ts
+++ src/routes/match.ts
@@ -3,9 +3,9 @@ import { Queue } from '../match/queue';
 import User from '../models/user';
 const router = Router();
 
-const rankedQueue = new Queue();
+const unrankedQueue = new Queue();
 // join ranked queue API
-router.post('/match/join', async (req, res) => {
+router.post('/join', async (req, res) => {
   const { username }: { username: string } = req.body;
   if (!username) {
     return res.status(400).send({ message: 'cad request' });
@@ -19,16 +19,16 @@ router.post('/match/join', async (req, res) => {
     return res.status(404).send({ message: 'can not find this player' });
   }
   // check if user is already in the queue
-  const inTheQueue = rankedQueue.get().find((item) => item.id === username);
+  const inTheQueue = unrankedQueue.get().find((item) => item.id === username);
   if (inTheQueue) {
     return res.status(400).send({ message: 'you are already in the queue' });
   }
-  rankedQueue.join(username);
+  unrankedQueue.join(username);
   return res.status(200).send({ message: 'join queue successfully' });
 });
 
 // leave ranked queue API
-router.post('/match/leave', async (req, res) => {
+router.post('/leave', async (req, res) => {
   const { username }: { username: string } = req.body;
   if (!username) {
     return res.status(400).send({ message: 'bad request' });
@@ -41,16 +41,16 @@ router.post('/match/leave', async (req, res) => {
   if (isPlayerExist.length == 0) {
     return res.status(404).send({ message: 'can not find playerID' });
   }
-  rankedQueue.leave(username);
+  unrankedQueue.leave(username);
   return res.status(200).send({ message: 'leave the queue successfully' });
 });
 
 // get queue or get first n's players
-router.get('/match/queue', (req, res) => {
+router.get('/queue', (req, res) => {
   const { quantity } = req.body;
   const result = isNaN(quantity)
-    ? rankedQueue.get()
-    : rankedQueue.getFirstNItems(quantity);
+    ? unrankedQueue.get()
+    : unrankedQueue.getFirstNItems(quantity);
   return res.status(200).send({ result });
 });
 
