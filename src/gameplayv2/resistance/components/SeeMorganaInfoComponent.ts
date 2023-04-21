7f64692c8c175aa8553267529a6780e488a4f8cf
Mengchen Ye <127305661+MengchenYe00000@users.noreply.github.com>
2023-04-21 20:51:38 +1000

    Add see() method for both seespy and seemorgana ts, and fix bugs in seespy

--- src/gameplayv2/resistance/components/SeeMorganaInfoComponent.ts
+++ src/gameplayv2/resistance/components/SeeMorganaInfoComponent.ts
@@ -2,11 +2,30 @@ export class SeeMorganaInfoComponent {
     private Morgana: string;
     private Role: string;
     private isEnabled: boolean;
+    private thisRoom: any;
 
-    constructor(Morgana: string, Role: string, isEnabled: boolean) {
+    constructor(Morgana: string, Role: string, isEnabled: boolean, thisRoom: any) {
         this.Morgana = Morgana;
         this.Role = Role;
         this.isEnabled = isEnabled;
+        this.thisRoom = thisRoom;
+    }
+
+    see(): { [username: string]: { roleTag: string } } {
+        if (this.thisRoom.gameStarted === true) {
+            const roleTag: { [username: string]: { roleTag: string } } = {};
+
+            for (let i = 0; i < this.thisRoom.playersInGame.length; i++) {
+                if (this.thisRoom.playersInGame[i].role === 'Morgana') {
+                    if (this.thisRoom.playersInGame[i].role !== 'Oberon') {
+                        roleTag[this.thisRoom.playersInGame[i].username] = { roleTag: 'Morgana' };
+                    }
+                }
+            }
+            return roleTag;
+        } else {
+            return {};
+        }
     }
 
     // Getter and setter for Morgana
