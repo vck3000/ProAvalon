982c388682619c0809b8acb31959562977961f43
RinaYoo <62081885+RinaYoo@users.noreply.github.com>
2023-04-23 00:16:39 +1000

    Created the countup timer when the users are in queue

--- src/views/components/queue/hydrate.tsx
+++ src/views/components/queue/hydrate.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { hydrate } from "react-dom";
-import MatchMakingModal from './index';
+import MatchLoading from './index';
 
 
-hydrate(<MatchMakingModal />, document.getElementById('matchMakingTimer'));
+hydrate(<MatchLoading />, document.getElementById('matchMakingTimer'));
--- src/views/components/queue/index.tsx
+++ src/views/components/queue/index.tsx
@@ -1,36 +1,103 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { hot } from 'react-hot-loader/root';
 import Modal from 'react-modal';
+import { subscribe, Queue } from '../../../match/queue';
 
 declare const currentOnlinePlayers: { displayUsername: string };
 
 Modal.setAppElement('#matchMakingTimer');
+type ButtonId = 'rankBtn' | 'unrankBtn';
 
-function MatchMakingModal() {
-  // This is boolean to know whether the user click the matchmaking button
-  const [modalIsClick, setModalIsClick] = useState(false);
+function MatchLoading() {
+  // All useStates
+  const [clickedButton, setClickedButton] = useState<ButtonId | null>(null);
+  const [buttonsDisabled, setButtonsDisabled] = useState(false);
+  const [showElement, setShowElement] = useState(false);
+  const [count, setCount] = useState(0);
 
-  function clickModal() {
-    setModalIsClick(true);
-    console.log("hi");
+  // Variables for timer
+  const minutes = Math.floor(count / 60);
+  const seconds = count % 60;
+  const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds
+    .toString()
+    .padStart(2, '0')}`;
+
+  // To create a countup timer
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  // Handlers
+  const handleClick = (button: ButtonId) => {
+    setClickedButton(button);
+    setShowElement(true);
+  };
+
+  const cancelQueue = () => {
+    setShowElement(false);
+    setCount(0);
+    setClickedButton(null);
+    setButtonsDisabled(false);
   }
 
-  function closeModal() {
-    setModalIsClick(false);
+  const btnStyle = (button: ButtonId) => {
+    const baseStyle: React.CSSProperties = { backgroundColor: 'transparent' };
+    if (clickedButton === button) {
+      baseStyle.backgroundColor = 'yellow';
+    }
+    if (buttonsDisabled) {
+      baseStyle.backgroundColor = 'transparent';
+      baseStyle.cursor = 'not-allowed';
+    }
+    return baseStyle;
+  }
+
+  const buttonProps = (button: ButtonId) => {
+    const props: React.ButtonHTMLAttributes<HTMLButtonElement> = {
+      style: btnStyle(button),
+      onClick: () => handleClick(button),
+    };
+    if (clickedButton && clickedButton !== button) {
+      props.disabled = true;
+    }
+    if (buttonsDisabled) {
+      props.disabled = true;
+    }
+    return props;
+  }
+
+  function Loading() {
+    return (
+      <div>
+        <p>You are in Queue:</p>
+        <h1>{formattedTime}</h1>
+        <button onClick={cancelQueue}>Cancel</button>
+      </div>
+    );
   }
 
   return (
     <div>
-      <button onClick={clickModal} className="matchmaking-btn btn btn-default">
+      <button
+        {...buttonProps('rankBtn')}
+        className="matchmaking-btn btn btn-default"
+      >
         Rank Game
       </button>
 
-      <Modal isOpen={modalIsClick} onRequestClose={closeModal}>
-        <p>Finding Match:</p>
+      <button
+        {...buttonProps('unrankBtn')}
+        className="matchmaking-btn btn btn-default"
+      >
+        Unranked Game
+      </button>
 
-      </Modal>
+      {showElement && <Loading />}
     </div>
   );
 }
 
-export default hot(MatchMakingModal);
+export default hot(MatchLoading);
