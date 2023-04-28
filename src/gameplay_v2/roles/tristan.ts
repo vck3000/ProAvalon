58ecbd19ae957684d62621ba683539e3e412cebf
Vinsensius Kelvin Wang <62001746+VKelvinWang@users.noreply.github.com>
2023-04-28 20:56:08 +1000

    push changes

--- src/gameplay_v2/roles/tristan.ts
+++ src/gameplay_v2/roles/tristan.ts
@@ -1,11 +1,12 @@
 import Entity from './entity';
 import { Alliance } from '../gameTypes';
 
-export default class Isolde extends Entity {
+export default class Tristan extends Entity {
   description: string;
 
   constructor() {
     super(Alliance.Resistance);
     this.description = 'Tristan and Isolde both see each other.';
+    //this.components.push(new seetristanIsoldeC());  
   }
 }
