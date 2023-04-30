c295bc27e765e8309d12b5589db8fc7b9999cef2
Vinsensius Kelvin Wang <62001746+VKelvinWang@users.noreply.github.com>
2023-04-30 22:42:08 +1000

    pushed comments for each system

---
+++ src/gameplay_v2/systems/initialPhaseS.ts
@@ -0,0 +1,4 @@
+//this phase will assign all players with random roles and send them back as gameData and sendData
+
+//An algorithm where every players are assign with different roles
+//An algorithm where a player will be given leader
