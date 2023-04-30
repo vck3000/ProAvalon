42729e89fc0b4fd730e89c18a21b404a0d2e1e63
Vinsensius Kelvin Wang <62001746+VKelvinWang@users.noreply.github.com>
2023-04-30 21:46:49 +1000

    pushed another change for declaring systems

--- src/gameplay_v2/systems/assassinationS.ts
+++ src/gameplay_v2/systems/assassinationS.ts
@@ -1,13 +1,16 @@
 import { GameData } from '../gameEngine';
 import { SendData, System } from './system';
 
-/*
+
 export class assassinationS implements System{
     run(gameData: GameData, sendData: SendData): void {
         const playersComponents = [];
         //check if there are 3 missions that are won by Resistance
+
+        //if(mission > 3 || mission === 3){
             //if so, then execute assassination phase where assassin selects the merlin
-                //if correct, end the game as SPY_WIN
-                //if not, then end the game with RESISTANCE_WIN
+            //if correct, end the game as SPY_WIN
+            //if not, then end the game with RESISTANCE_WIN
+        
     }
-}*/
+}
