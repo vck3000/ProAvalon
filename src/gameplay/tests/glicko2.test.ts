b62c9f19762d18e9e3f79b8a0b9a2276e4a710e4
Pai Qu <paqu2695@uni.sydney.edu.au>
2023-05-08 16:51:06 +1000

    Modify tests for Glicko-2

--- src/gameplay/tests/glicko2.test.ts
+++ src/gameplay/tests/glicko2.test.ts
@@ -1,79 +1,213 @@
-import Glicko2 from "../glicko2";
-import type { IUser } from "../types";
-import { TeamEnum } from "../types";
-import { IRatingPeriodGameRecord } from "../../models/types";
+import Glicko2 from '../glicko2';
+import { TeamEnum } from '../types';
+import User from '../../models/user';
+import Rank from '../../models/rank';
+import RatingPeriodGameRecord from '../../models/RatingPeriodGameRecord';
+import mongoose from 'mongoose';
+import { Types } from 'mongoose';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+// const mockingoose = require('mockingoose');
 
 describe('Glicko-2', () => {
-  let playerOne: IUser, games: IRatingPeriodGameRecord[], glicko2: Glicko2;
+  const user1Id = new Types.ObjectId('64562e343eb03ca9ac251655');
+  const user2Id = new Types.ObjectId('64562e906fddefa87680cc3f');
+  const user3Id = new Types.ObjectId('64562e96ce0da57d19e89002');
+  const user4Id = new Types.ObjectId('64562e9d4af24569779941ac');
+  const user5Id = new Types.ObjectId('64562ea4d18148c6be4ceeec');
+  const user6Id = new Types.ObjectId('64562eaa96d5b620000ea3de');
+  const user7Id = new Types.ObjectId('64563ed1cc26b8ffd49cd9f9');
+  const user8Id = new Types.ObjectId('64587b6e8a1ea4182899427c');
+  const glicko2 = new Glicko2();
 
-  beforeAll(() => {
-    glicko2 = new Glicko2();
+  beforeAll(async () => {
+    // Connect to Mongodb
+    const dbLoc =
+      'mongodb://root:password@localhost:27017/proavalon?authSource=admin';
+    await mongoose.connect(dbLoc, {
+      retryWrites: false,
+    });
 
-    playerOne = {
-      username: 'playerone',
-      playerRating: 1500,
-      ratingDeviation: 200,
-      ratingVolatility: 0.06,
-    };
-
-    games = [
+    const players = [
       {
-        timeGameStarted: new Date(),
-        timeGameFinished: new Date(),
-        winningTeam: 'Resistance',
-        spyTeam: [],
-        resistanceTeam: [playerOne.username],
-        numberOfPlayers: 6,
-        roomCreationType: '',
-        
-        avgRating: 1400,
-        avgRd: 30,
+        _id: user1Id,
+        username: 'player1',
       },
       {
-        timeGameStarted: new Date(),
-        timeGameFinished: new Date(),
-        winningTeam: 'Resistance',
-        spyTeam: [playerOne.username],
-        resistanceTeam: [],
-        numberOfPlayers: 6,
-        roomCreationType: '',
-
-        avgRating: 1550,
-        avgRd: 100,
+        _id: user2Id,
+        username: 'player2',
       },
       {
-        timeGameStarted: new Date(),
-        timeGameFinished: new Date(),
-        winningTeam: 'Spy',
-        spyTeam: [],
-        resistanceTeam: [playerOne.username],
-        numberOfPlayers: 6,
-        roomCreationType: '',
+        _id: user3Id,
+        username: 'player3',
+      },   
+      {
+        _id: user4Id,
+        username: 'player4',
+      },
+      {
+        _id: user5Id,
+        username: 'player5',
+      },   
+      {
+        _id: user6Id,
+        username: 'player6',
+      },   
+      {
+        _id: user7Id,
+        username: 'player7',
+      },   
+      {
+        _id: user8Id,
+        username: 'player8',
+      },   
+    ];
 
-        avgRating: 1700,
-        avgRd: 300,
+    const ranks = [
+      {
+        userId: user1Id,
+        seasonNumber: 0,
+        playerRating: 1500,
+        rd: 200,
+        volatility: 0.06,
+      },
+      {
+        userId: user2Id,
+        seasonNumber: 0,
+        playerRating: 1700,
+        rd: 170,
+        volatility: 0.06,
+      },
+      {
+        userId: user3Id,
+        seasonNumber: 0,
+        playerRating: 1400,
+        rd: 30,
+        volatility: 0.06,
+      },
+      {
+        userId: user4Id,
+        seasonNumber: 0,
+        playerRating: 1400,
+        rd: 30,
+        volatility: 0.06,
+      },
+      {
+        userId: user5Id,
+        seasonNumber: 0,
+        playerRating: 1400,
+        rd: 30,
+        volatility: 0.06,
+      },
+      {
+        userId: user6Id,
+        seasonNumber: 0,
+        playerRating: 2600,
+        rd: 1110,
+        volatility: 0.06,
+      },
+      {
+        userId: user7Id,
+        seasonNumber: 0,
+        playerRating: 1400,
+        rd: 30,
+        volatility: 0.06,
+      },
+      {
+        userId: user8Id,
+        seasonNumber: 0,
+        playerRating: 1500,
+        rd: 200,
+        volatility: 0.06,
       },
     ];
+
+    const games = [
+      {
+        timeGameFinished: new Date(),
+        winningTeam: TeamEnum.SPY,
+        spyTeam: [user1Id, user2Id],
+        resistanceTeam: [user3Id, user4Id, user5Id, user7Id],
+        roomCreationType: '',
+      },
+      {
+        timeGameFinished: new Date(),
+        winningTeam: TeamEnum.SPY,
+        spyTeam: [user2Id, user4Id],
+        resistanceTeam: [user1Id, user3Id, user5Id, user6Id, user7Id],
+        roomCreationType: '',
+      },
+      {
+        timeGameFinished: new Date(),
+        winningTeam: TeamEnum.RESISTANCE,
+        spyTeam: [user1Id, user2Id],
+        resistanceTeam: [user3Id, user4Id, user5Id, user6Id],
+        roomCreationType: '',
+      },
+      {
+        timeGameFinished: new Date(),
+        winningTeam: TeamEnum.RESISTANCE,
+        spyTeam: [user2Id, user3Id],
+        resistanceTeam: [user4Id, user5Id, user6Id, user7Id],
+        roomCreationType: '',
+      },
+      {
+        timeGameFinished: new Date(),
+        winningTeam: TeamEnum.SPY,
+        spyTeam: [user2Id, user3Id],
+        resistanceTeam: [user4Id, user5Id, user6Id, user7Id],
+        roomCreationType: '',
+      },
+      {
+        timeGameFinished: new Date(),
+        winningTeam: TeamEnum.RESISTANCE,
+        spyTeam: [user7Id, user4Id],
+        resistanceTeam: [user5Id, user5Id, user6Id],
+        roomCreationType: '',
+      },
+    ];
+
+    await User.insertMany(players);
+    await Rank.insertMany(ranks);
+    await RatingPeriodGameRecord.insertMany(games);
   });
 
-  it('should use the game data from rating period to calculate new rating correctly', () => {
-    const { playerRating } = glicko2.updateRatingsByPlayer(playerOne, games);
-    expect(playerRating).toBeCloseTo(1464.05);
+  afterAll(async () => {
+    await User.deleteMany({});
+    await Rank.deleteMany({});
+    await RatingPeriodGameRecord.deleteMany({});
+    mongoose.disconnect();
   });
 
-  it('should use the game data from rating period to calculate new RD correctly', () => {
-    const { ratingDeviation } = glicko2.updateRatingsByPlayer(playerOne, games);
-    expect(ratingDeviation).toBeCloseTo(151.52);
+  it('calculates the correct ratings, rd, and volatility for a user after the rating period ends', async () => {
+    await glicko2.updateRatingsByUser(user1Id);
+    const newRank = await Rank.findOne({
+      userId: user1Id,
+      seasonNumber: 1,
+    });
+
+    expect(newRank.playerRating).toBeCloseTo(1464.05);
+    expect(newRank.rd).toBeCloseTo(151.52);
+    expect(newRank.volatility).toBeCloseTo(0.05999);
   });
 
-  it('should only update RD for inactive player', () => {
-    const { playerRating } = glicko2.updateRatingsByPlayer(playerOne, []);
-    expect(playerRating).toBe(playerOne.playerRating);
+  it('computes the correct ratings for an inactive player', async () => {
+    const oldRank = await Rank.findOne({
+      userId: user8Id,
+      seasonNumber: 0,
+    });
+
+    await glicko2.updateRatingsByUser(user8Id);
+    const newRank = await Rank.findOne({
+      userId: user8Id,
+      seasonNumber: 1,
+    });
+    expect(newRank.playerRating).toBe(oldRank.playerRating);
+    expect(newRank.rd).toBeGreaterThan(oldRank.rd);
+    expect(newRank.volatility).toBe(oldRank.volatility);
   });
 
-  it('should update the RD correctly for inactive player', () => {
-    const { ratingDeviation } = glicko2.updateRatingsByPlayer(playerOne, []);
-    const phi = playerOne.ratingDeviation / 173.7178;
-    expect(ratingDeviation).toBe(173.7178 * Math.sqrt(phi ** 2 + playerOne.ratingVolatility ** 2));
-  });
+  // it('should update the season number after updating all users', () => {
+
+  // })
 });
+
