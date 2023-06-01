b3c993c822c848991a12428ff453e8371a340be5
cwei9203 <cwei9203@uni.sydney.edu.au>
2023-06-01 13:27:53 +1000

    change eloConstants

--- src/elo/constants/eloConstants.ts
+++ src/elo/constants/eloConstants.ts
@@ -4,14 +4,19 @@ const eloConstants = {
     DEFAULT_RD: 350,
     DEFAULT_VOL: 0.06,
     PROVISION_GAMES : 20,
+    
+    //Deduct score for leave
+    LEAVE_PENALTY : 100,
+} as const;
+
+const rankBracket = {
     BRONZE_BASE : 1300,
     SILVER_BASE : 1400,
     GOLD_BASE : 1550,
     PLATINUM_BASE : 1700,
     DIAMOND_BASE : 1800,
     CHAMPION_BASE : 1900,
-    //Deduct score for leave
-    LEAVE_PENALTY : 100,
 } as const;
 
-export default eloConstants;
+export { eloConstants, rankBracket };
+
