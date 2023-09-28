4c4df00802b6d6aa4b695e7d383231b24e8d70da
Victor Kuo <vck3000@gmail.com>
2023-09-29 08:00:41 +1000

    Empty devs array

--- src/modsadmins/developers.ts
+++ src/modsadmins/developers.ts
@@ -1,6 +1,6 @@
 // all in lower case
 // change the devsArray elements to actual developer names
-export const devsArray = ['c0nradd', 'cin333', 'ayad318'];
+export const devsArray: string[] = [];
 
 export function isDev(username: string): boolean {
   return devsArray.includes(username.toLowerCase());
