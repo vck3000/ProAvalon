7a8dd78aba58f2fee63497ddba6f443abc9ba7c9
Victor Kuo <vck3000@gmail.com>
2023-09-29 08:00:03 +1000

    Update tourney organisers

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,20 +1,11 @@
 // all in lower case
 // TO = Tournament Organizer
 export const TOsArray = [
-  // Manu's TOs
-  'manuavalon',
-  'tyrrox',
   'glorious',
-  'goofy',
-  'starkrush',
-  'secretaccount8594',
-  'pat',
-
-  // Ref-rain's TOs
-  'ref-rain',
   'wolfbloodwitch',
   'altwoa',
   'bremen',
+  'pat',
 ];
 
 export function isTO(username: string): boolean {
