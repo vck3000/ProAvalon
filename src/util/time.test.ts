1ae4b033faffec52ae030f4eb430f80cc33b7bd6
Kevin <kevinwong328@gmail.com>
2024-03-12 21:18:53 +1100

    Edits to formatting

--- src/util/time.test.ts
+++ src/util/time.test.ts
@@ -1,18 +1,18 @@
-import {millisToStr} from "./time";
+import { millisToStr } from './time';
+
 describe('TimeConverter', () => {
-    it('Converts time correctly', () => {
+  it('Converts time correctly', () => {
+    // Test seconds displayed correctly
+    expect(millisToStr(1000)).toEqual('00:01');
+    expect(millisToStr(30000)).toEqual('00:30');
+    expect(millisToStr(2050)).toEqual('00:02');
 
-        //Test seconds displayed correctly
-        expect(millisToStr(1000)).toEqual(("00:01"));
-        expect(millisToStr(30000)).toEqual(("00:30"));
-        expect(millisToStr(2050)).toEqual(("00:02"));
+    // Test minutes displayed correctly
+    expect(millisToStr(60000)).toEqual('01:00');
+    expect(millisToStr(660000)).toEqual('11:00');
+    expect(millisToStr(61005)).toEqual('01:01');
 
-        //Test minutes displayed correctly
-        expect(millisToStr(60000)).toEqual("01:00");
-        expect(millisToStr(660000)).toEqual("11:00");
-        expect(millisToStr(61005)).toEqual("01:01");
-
-        //Test min + sec displayed correctly
-        expect(millisToStr(62000)).toEqual("01:02");
-    })
+    // Test min + sec displayed correctly
+    expect(millisToStr(62000)).toEqual('01:02');
+  });
 });
--- src/util/time.ts
+++ src/util/time.ts
@@ -1,21 +1,21 @@
-export function millisToStr(millis: number) : string {
+export function millisToStr(millis: number): string {
+  let minutes: number = Math.floor(millis / (1000 * 60));
+  let seconds: number = Math.floor((millis / 1000) % 60);
 
-    let minutes: number = Math.floor(millis/ (1000 * 60) );
-    let seconds: number = Math.floor((millis / 1000) % 60);
-    let formattedSeconds: string;
-    let formattedMinutes: string;
+  let formattedSeconds: string;
+  let formattedMinutes: string;
 
-    if (seconds < 10) {
-        formattedSeconds = "0" + seconds.toString();
-    } else {
-        formattedSeconds = seconds.toString();
-    }
+  if (seconds < 10) {
+    formattedSeconds = '0' + seconds.toString();
+  } else {
+    formattedSeconds = seconds.toString();
+  }
 
-    if (minutes < 10) {
-        formattedMinutes = "0" + minutes.toString();
-    } else {
-        formattedMinutes = minutes.toString();
-    }
+  if (minutes < 10) {
+    formattedMinutes = '0' + minutes.toString();
+  } else {
+    formattedMinutes = minutes.toString();
+  }
 
-    return `${formattedMinutes}:${formattedSeconds}`;
+  return `${formattedMinutes}:${formattedSeconds}`;
 }
