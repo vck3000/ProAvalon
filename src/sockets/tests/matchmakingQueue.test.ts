6455ab7819adf8b89d252b9deee4724a7b03b48b
Kevin <kevinwong328@gmail.com>
2024-03-28 16:03:21 +1100

    Removed redundant test cases

--- src/sockets/tests/matchmakingQueue.test.ts
+++ src/sockets/tests/matchmakingQueue.test.ts
@@ -233,30 +233,4 @@ describe('MatchmakingQueue', () => {
       expect(combinations).toEqual(expected);
     });
   });
-
-  describe('DynamicMatchmaking', () => {
-    it('Can wait 10 seconds before finding match', () => {
-      for (let i = 1; i <= 6; i++) {
-        matchmakingQueue.addUser(getDefaultQueueEntry(i.toString()));
-      }
-
-      expect(matchFoundCallback).not.toHaveBeenCalled();
-      matchmakingQueue.addUser(getDefaultQueueEntry('7'));
-      expect(matchFoundCallback).not.toHaveBeenCalled();
-
-      jest.advanceTimersByTime(MM_JOIN_WINDOW);
-
-      const expectUsernames = ['1', '2', '3', '4', '5', '6', '7'];
-      expect(matchFoundCallback).toHaveBeenCalledWith(expectUsernames);
-    });
-
-    it('Will not delay finding a match when 8ppl are in queue', () => {
-      for (let i = 1; i <= 8; i++) {
-        matchmakingQueue.addUser(getDefaultQueueEntry(i.toString()));
-      }
-
-      const expectUsernames = ['1', '2', '3', '4', '5', '6', '7', '8'];
-      expect(matchFoundCallback).toHaveBeenCalledWith(expectUsernames);
-    });
-  });
 });
