b039260c2b53ba06159adf9ebd69af157e92911b
Victor Kuo <vck3000@gmail.com>
2024-04-04 23:18:09 +1100

    Slight refactor to roll

--- src/sockets/commands/user/roll.ts
+++ src/sockets/commands/user/roll.ts
@@ -6,26 +6,29 @@ export const roll: Command = {
   command: 'roll',
   help: '/roll <optional number>: Returns a random number between 1 and 10 or 1 and optional number.',
   run: async (args: string[], socket: SocketUser) => {
-    console.log(args.length);
     if (args.length > 2) {
       sendReplyToCommand(socket, 'Enter just one number.');
       return;
     }
-    if (args[1]) {
+
+    // Default value
+    let maxNum = 10;
+
+    // They didn't give a value in
+    if (args.length === 2) {
       const optionalNumber = parseInt(args[1]);
 
       if (isNaN(optionalNumber) || optionalNumber.toString() !== args[1]) {
         sendReplyToCommand(socket, 'That is not a valid number!');
         return;
-      } else {
-        sendReplyToCommand(
-          socket,
-          (Math.floor(Math.random() * parseInt(args[1])) + 1).toString(),
-        );
-        return;
       }
+
+      maxNum = optionalNumber;
     }
 
-    sendReplyToCommand(socket, (Math.floor(Math.random() * 10) + 1).toString());
+    sendReplyToCommand(
+      socket,
+      (Math.floor(Math.random() * maxNum) + 1).toString(),
+    );
   },
 };
