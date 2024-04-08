0c5e3bd3075c1ed06c91faa351f399ccf3aeada8
kev306 <118262634+kev306@users.noreply.github.com>
2024-04-08 19:11:41 +1000

    User command refactor (#651)

---
+++ src/sockets/commands/user/removeblacklist.ts
@@ -0,0 +1,36 @@
+import { Command } from '../types';
+import { SocketUser } from '../../types';
+import { sendReplyToCommand } from '../../sockets';
+import { IUser } from '../../../gameplay/types';
+
+export const removeblacklist: Command = {
+  command: 'removeblacklist',
+  help: '/removeblacklist <username>: Removes a user from your blacklist.',
+  run: async (args: string[], socket: SocketUser) => {
+    if (args.length < 2) {
+      sendReplyToCommand(socket, 'Please specify a username.');
+      return;
+    }
+
+    const usernameToBlacklist = args[1].toLowerCase();
+    const user = socket.request.user;
+
+    const index = user.matchmakingBlacklist.indexOf(usernameToBlacklist);
+    if (index === -1) {
+      sendReplyToCommand(
+        socket,
+        `${usernameToBlacklist} was not on your blacklist.`,
+      );
+      return;
+    }
+    user.matchmakingBlacklist.splice(index, 1);
+    socket.request.user.markModified('matchmakingBlacklist');
+    await socket.request.user.save();
+
+    sendReplyToCommand(
+      socket,
+      `Removed ${usernameToBlacklist} from your blacklist.`,
+    );
+    return;
+  },
+};
