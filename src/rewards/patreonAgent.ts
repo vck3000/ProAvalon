d7e247eadb36e0b1cec5d9041ad84e9d1aeb6459
Kevin <kevinwong328@gmail.com>
2024-05-01 18:32:47 +1000

    Patreon endpoints now successfully accessible

--- src/rewards/patreonAgent.ts
+++ src/rewards/patreonAgent.ts
@@ -16,15 +16,12 @@ export class PatreonAgent {
       client_id: this.clientId,
       redirect_uri: this.redirectUri,
       state: 'chill', // TODO-kev: Is this needed?
-      scope: 'identity identity.memberships',
+      scope: 'identity',
     },
   });
 
   public async registerPatreon(code: string) {
     const userDetails = await this.getUserDetails(code);
-    // await this.getCampaignMembers();
-    // await this.getCampaignMembers();
-    // await this.memberById();
 
     // Check if you can find the PatreonId
     // If you cant find it then create one
@@ -34,87 +31,99 @@ export class PatreonAgent {
     // If expired then update
   }
 
-  public async getCreatorCampaign() {
-    // const tokens = await this.getTokens(code);
-    const url =
-      'https://www.patreon.com/api/oauth2/v2/campaigns?fields%5Bmember%5D=full_name,patron_status,will_pay_amount_cents';
-    // console.log(tokens.access_token);
-    const headers = {
-      Authorization: `Bearer ${this.patreonCreatorToken}`,
-    };
-
-    const response = await axios.get(url, { headers });
-
-    console.log(response.data);
-    return response.data;
-  }
-
-  public async getCampaignMembers() {
-    // Still have to test if opened from ProNub account. None of the members or campaign info show
-
-    const url = `https://www.patreon.com/api/oauth2/v2/campaigns/${process.env.patreon_campaign}/members`;
-    const headers = {
-      Authorization: `Bearer ${this.patreonCreatorToken}`,
-    };
-
-    const response = await axios.get(url, { headers });
-
-    console.log(response.data);
-    return response.data;
-  }
-
-  public async memberById() {
-    // Still have to test if opened from ProNub account. None of the members or campaign info show
-
-    const url = `https://www.patreon.com/api/oauth2/v2/members/126667873`;
-    const headers = {
-      Authorization: `Bearer ${this.patreonCreatorToken}`,
-    };
-
-    const response = await axios.get(url, { headers });
-
-    console.log(response.data);
-    return response.data;
-  }
+  // public async getCreatorCampaign() {
+  //   // const tokens = await this.getTokens(code);
+  //   const url =
+  //     'https://www.patreon.com/api/oauth2/v2/campaigns?fields%5Bmember%5D=full_name,patron_status,will_pay_amount_cents&fields%5Btier%5D=description,title,patron_count';
+  //   // console.log(tokens.access_token);
+  //   const headers = {
+  //     Authorization: `Bearer ${this.patreonCreatorToken}`,
+  //   };
+  //
+  //   const response = await axios.get(url, { headers });
+  //
+  //   console.log(response.data);
+  //   return response.data;
+  // }
+  //
+  // public async getCampaignMembers() {
+  //   // Still have to test if opened from ProNub account. None of the members or campaign info show
+  //
+  //   const url = `https://www.patreon.com/api/oauth2/v2/campaigns/${process.env.patreon_campaign}/members?include=currently_entitled_tiers`;
+  //   const headers = {
+  //     Authorization: `Bearer ${this.patreonCreatorToken}`,
+  //   };
+  //
+  //   const response = await axios.get(url, { headers });
+  //
+  //   console.log(response.data);
+  //   return response.data;
+  // }
+  //
+  // public async memberById() {
+  //   // Still have to test if opened from ProNub account. None of the members or campaign info show
+  //
+  //   const url = `https://www.patreon.com/api/oauth2/v2/members/126667873`;
+  //   const headers = {
+  //     Authorization: `Bearer ${this.patreonCreatorToken}`,
+  //   };
+  //
+  //   const response = await axios.get(url, { headers });
+  //
+  //   console.log(response.data);
+  //   return response.data;
+  // }
 
   public async getUserDetails(code: string) {
     const tokens = await this.getTokens(code);
+    // TODO-kev: Do we want their email instead of url and even name?
     const url =
-      'https://www.patreon.com/api/oauth2/v2/identity?include=memberships&fields%5Buser%5D=full_name&fields%5Bmember%5D=lifetime_support_cents';
+      'https://www.patreon.com/api/oauth2/v2/identity?include=memberships,memberships.currently_entitled_tiers&fields%5Buser%5D=full_name&fields%5Bmember%5D=lifetime_support_cents,patron_status,campaign_lifetime_support_cents&fields%5Btier%5D=title';
     const headers = {
       Authorization: `Bearer ${tokens.access_token}`,
     };
 
     const response = await axios.get(url, { headers });
 
-    // const getUserDetailsUrl =
-    //   'https://www.patreon.com/api/oauth2/v2/identity?include=memberships&fields%5Buser%5D=full_name&fields%5Bmember%5D=patron_status,will_pay_amount_cents,campaign_lifetime_support_cents';
-    //
-    // const response = await axios.get(getUserDetailsUrl, {
-    //   headers: {
-    //     Authorization: `Bearer ${tokens.access_token}`,
-    //   },
-    // }
+    const patreonId = response.data.data.id;
+    const fullName = response.data.data.attributes.full_name;
+    const userAccessToken = tokens.access_token;
+    const userAccessTokenExpiry = tokens.expires_in;
+    let currentTier = '';
+    let patreonStatus = null;
+    let patreonTotalCents = 0;
 
-    console.log(response.data);
-    const userId = response.data.data.id;
-    console.log(userId);
+    if (response.data.included) {
+      if (
+        response.data.included.length >= 1 &&
+        response.data.included.length <= 2
+      ) {
+        const patreonMemberDetails = response.data.included[0].attributes;
+        const patreonTierDetails = response.data.included[1].attributes;
 
-    // const url2 = `https://www.patreon.com/api/oauth2/v2/members/${userId}`;
-    // const params2 = {
-    //   include: 'currently_entitled_tiers',
-    //   fields: {
-    //     member: 'full_name',
-    //   },
-    // };
-    // const headers2 = {
-    //   Authorization: `Bearer ${this.patreonCreatorToken}`,
-    // };
-    //
-    // // @ts-ignore
-    // const response2 = await axios.get(url2, { params2, headers2 });
-    //
-    // console.log(response2.data);
+        patreonStatus = patreonMemberDetails.patron_status;
+        patreonTotalCents =
+          patreonMemberDetails.campaign_lifetime_support_cents;
+        currentTier = patreonTierDetails.title;
+      } else if (response.data.included.length > 2) {
+        // TODO-kev: Will need to test this. What happens if a user upgrades their plan?
+        throw new Error(
+          `Unexpected number of Patreon memberships received. name="${fullName}" memberships="${response.data.included}"`,
+        );
+      }
+    }
+
+    // TODO-kev: Design decision on when a patron is considered a valid patron? When they paid etc
+
+    console.log('====================');
+    console.log(`patreonId: ${patreonId}`);
+    console.log(`fullName: ${fullName}`);
+    console.log(`userAccessToken: ${userAccessToken}`);
+    console.log(`userAccessTokenExpiry: ${userAccessTokenExpiry}`);
+    console.log(`currentTier: ${currentTier}`);
+    console.log(`patreonStatus: ${patreonStatus}`);
+    console.log(`patreonTotalCents: ${patreonTotalCents}`);
+    console.log('====================');
 
     return response.data;
   }
@@ -145,9 +154,5 @@ export class PatreonAgent {
     // }
 
     return response.data;
-  }
-
-  public async getPatreonQuery(patreonId: string) {
-    return PatreonId.findOne({ id: patreonId });
   }
 }
