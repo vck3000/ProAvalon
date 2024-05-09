a96d408b7b795d38bea9716b014d759bd38f0f2b
Victor Kuo <vck3000@gmail.com>
2024-05-10 00:32:27 +1000

    Fixes

--- src/views/components/matchmaking/matchmakingButton.tsx
+++ src/views/components/matchmaking/matchmakingButton.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { Socket } from 'socket.io';
 
-// @ts-ignore
-const socket: Socket = socket_;
+let socket: Socket = undefined;
 
 export function MatchmakingButton() {
   const [joined, setJoined] = useState(false);
   const [queueButtonText, setQueueButtonText] = useState('Join Queue');
 
   useEffect(() => {
-    socket.on('queueReply', (data: any) => {
+    // @ts-ignore
+    socket = socket_;
+
+    socket.on('queueReply', (data: { joined: boolean }) => {
       setJoined(data.joined);
       setQueueButtonText(data.joined ? 'Leave Queue' : 'Join Queue');
     });
