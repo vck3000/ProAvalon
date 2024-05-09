fc8d35b3a4225a4dc134a8c5cb8540a158542879
Victor Kuo <vck3000@gmail.com>
2024-05-10 00:39:09 +1000

    Add extra join buttons

--- src/views/components/matchmaking/hydrate.tsx
+++ src/views/components/matchmaking/hydrate.tsx
@@ -3,3 +3,5 @@ import { hydrate } from 'react-dom';
 import Matchmaking from './index';
 
 hydrate(<Matchmaking />, document.getElementById('matchmakingDiv'));
+hydrate(<Matchmaking />, document.getElementById('matchmakingDivWithinRoom1'));
+hydrate(<Matchmaking />, document.getElementById('matchmakingDivWithinRoom2'));
