43cf63d6fa30821a858f9b2400c24341b44ef6d5
Kevin <kevinwong328@gmail.com>
2024-05-10 18:38:46 +1000

    Added mod command to set a users resistance avatar

--- src/sockets/commands/mod/index.ts
+++ src/sockets/commands/mod/index.ts
@@ -17,6 +17,7 @@ import { miplinkedaccs } from './miplinkedaccs';
 import { mtogglepause } from './mtogglepause';
 import { mrevealallroles } from './mrevealallroles';
 import { mforcemove } from './mforcemove';
+import { msetresavatar } from './msetresavatar';
 
 export const modCommands: Commands = {
   [m.command]: m,
@@ -29,6 +30,7 @@ export const modCommands: Commands = {
   [mtoggleregistration.command]: mtoggleregistration,
   [mtempenableregistration.command]: mtempenableregistration,
   [mwhisper.command]: mwhisper,
+  [msetresavatar.command]: msetresavatar,
   [mremoveavatar.command]: mremoveavatar,
   [mclose.command]: mclose,
   [mannounce.command]: mannounce,
