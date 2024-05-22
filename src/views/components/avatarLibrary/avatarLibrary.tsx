179d96de8a9df3760affcd1b5ab025cbbb205f49
Kevin <kevinwong328@gmail.com>
2024-05-22 16:06:32 +1000

    Added carousel views to modview

--- src/views/components/avatarLibrary/avatarLibrary.tsx
+++ src/views/components/avatarLibrary/avatarLibrary.tsx
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from 'react';
-import { S3AvatarSet } from '../../../clients/s3/S3Agent';
+import { AllApprovedAvatars, S3AvatarSet } from '../../../clients/s3/S3Agent';
 import Swal from 'sweetalert2';
+import Carousel from 'react-multi-carousel';
+import { AllAvatarsRouteReturnType } from '../../../routes/profile/avatarRoutes';
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
 export function AvatarLibrary() {
   const [usernameToSearchAvatar, setUsernameToSearchAvatar] = useState<
     string | null
   >(null);
+  const [approvedAvatarsNotInLibrary, setApprovedAvatarsNotInLibrary] =
+    useState<S3AvatarSet[] | null>(null);
   const [avatarLibrary, setAvatarLibrary] = useState<S3AvatarSet[] | null>(
     null,
   );
+
+  useEffect(() => {
+    require('react-multi-carousel/lib/styles.css');
+  }, []);
 
   const handleGetAvatars = async () => {
     if (!usernameToSearchAvatar) {
@@ -24,9 +47,9 @@ export function AvatarLibrary() {
     );
 
     if (response.status === 200) {
-      const data: S3AvatarSet[] = await response.json();
-      setAvatarLibrary(data);
-      console.log(data);
+      const data: AllApprovedAvatars = await response.json();
+      setAvatarLibrary(data.avatarLibrary);
+      setApprovedAvatarsNotInLibrary(data.approvedAvatarsNotInLibrary);
     } else {
       Swal.fire({ title: await response.text(), icon: 'error' });
     }
@@ -60,6 +83,83 @@ export function AvatarLibrary() {
           Get Avatars
         </button>
       </div>
+
+      <h3>
+        <u>User 1's avatar library:</u>
+      </h3>
+      <Carousel
+        responsive={responsive}
+        containerClass="carousel-container"
+        showDots={true}
+        keyBoardControl={true}
+        removeArrowOnDeviceType={['mobile']}
+        centerMode={true}
+      >
+        {!avatarLibrary || avatarLibrary.length === 0 ? (
+          <p className={'alignCenter'}>
+            Your avatar library is currently empty.
+          </p>
+        ) : (
+          avatarLibrary.map((avatarSet) => (
+            <div key={avatarSet.avatarSetId} className="avatarSet">
+              <h3 className="avatarTitle">Avatar {avatarSet.avatarSetId}</h3>
+              <img
+                src={avatarSet.resLink}
+                alt={`Resistance avatar ${avatarSet.avatarSetId}`}
+                className="avatarImg"
+                draggable={false}
+              />
+              <img
+                src={avatarSet.spyLink}
+                alt={`Spy avatar ${avatarSet.avatarSetId}`}
+                className="avatarImg"
+                draggable={false}
+              />
+            </div>
+          ))
+        )}
+      </Carousel>
+
+      <h3>
+        <u>User 1's approved avatars not in their library:</u>
+      </h3>
+      <Carousel
+        responsive={responsive}
+        containerClass="carousel-container"
+        showDots={true}
+        keyBoardControl={true}
+        removeArrowOnDeviceType={['mobile']}
+        centerMode={true}
+      >
+        {!approvedAvatarsNotInLibrary ||
+        approvedAvatarsNotInLibrary.length === 0 ? (
+          <p className={'alignCenter'}>
+            Your avatar library is currently empty.
+          </p>
+        ) : (
+          approvedAvatarsNotInLibrary.map((avatarSet) => (
+            <div key={avatarSet.avatarSetId} className="avatarSet">
+              <h3 className="avatarTitle">Avatar {avatarSet.avatarSetId}</h3>
+              <img
+                src={avatarSet.resLink}
+                alt={`Resistance avatar ${avatarSet.avatarSetId}`}
+                className="avatarImg"
+                draggable={false}
+              />
+              <img
+                src={avatarSet.spyLink}
+                alt={`Spy avatar ${avatarSet.avatarSetId}`}
+                className="avatarImg"
+                draggable={false}
+              />
+            </div>
+          ))
+        )}
+      </Carousel>
+
+      <br />
+      {/*TODO-kev: Consider adding a clear button to clear input*/}
+      <p>Add in a way to swap between avatars here</p>
     </div>
   );
 }
