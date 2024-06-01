7b2c26d4d38fc234e128036fac9f8517169fb527
Kevin <kevinwong328@gmail.com>
2024-06-02 00:46:31 +1000

    Merge master

--- src/views/components/avatarHome/hydrate.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { hydrate } from 'react-dom';
-import AvatarHome from './index';
-
-hydrate(<AvatarHome />, document.getElementById('avatarHomeDiv'));
--- src/views/components/avatarHome/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { hot } from 'react-hot-loader/root';
-import { AvatarHome } from './avatarHome';
-
-export default hot(() => <AvatarHome />);
