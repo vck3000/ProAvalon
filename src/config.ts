0cbe508277ca944a25bf513f0098853c7c9d9eaf
Kevin <kevinwong328@gmail.com>
2024-06-15 01:02:07 +1000

    Added a check for test environments

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 const VALID_ENVIRONMENTS: Set<string> = new Set(['local', 'staging', 'prod']);
 
-// TODO-kev: Should i import this into app.ts?
 class Config {
   private readonly env: string;
   private readonly nodeEnv: string;
@@ -34,29 +33,32 @@ class Config {
   private readonly discordModRoleId: string;
 
   constructor() {
-    if (!VALID_ENVIRONMENTS.has(process.env.ENV)) {
-      // TODO-kev: Prefer the console.error then process exit or throw an error?
-      console.error(`Bad environment variable given: ${process.env.ENV}`);
-      throw new Error(`Invalid settings: ENV=${process.env.ENV}`);
-      process.exit(1);
-    }
+    // Run validation checks outside test environment
+    if (process.env.NODE_ENV !== 'test') {
+      if (!VALID_ENVIRONMENTS.has(process.env.ENV)) {
+        // TODO-kev: Prefer the console.error then process exit or throw an error?
+        console.error(`Bad environment variable given: ${process.env.ENV}`);
+        throw new Error(`Invalid settings: ENV=${process.env.ENV}`);
+        process.exit(1);
+      }
 
-    if (
-      process.env.ENV === 'staging' &&
-      process.env.S3_BUCKET_NAME !== 'proavalon-staging'
-    ) {
-      throw new Error(
-        `Invalid settings: ENV=staging S3_BUCKET_NAME=${process.env.S3_BUCKET_NAME}`,
-      );
-    }
+      if (
+        process.env.ENV === 'staging' &&
+        process.env.S3_BUCKET_NAME !== 'proavalon-staging'
+      ) {
+        throw new Error(
+          `Invalid settings: ENV=staging S3_BUCKET_NAME=${process.env.S3_BUCKET_NAME}`,
+        );
+      }
 
-    if (
-      process.env.ENV === 'prod' &&
-      process.env.S3_BUCKET_NAME !== 'proavalon'
-    ) {
-      throw new Error(
-        `Invalid settings: ENV=prod S3_BUCKET_NAME=${process.env.S3_BUCKET_NAME}`,
-      );
+      if (
+        process.env.ENV === 'prod' &&
+        process.env.S3_BUCKET_NAME !== 'proavalon'
+      ) {
+        throw new Error(
+          `Invalid settings: ENV=prod S3_BUCKET_NAME=${process.env.S3_BUCKET_NAME}`,
+        );
+      }
     }
 
     this.env = process.env.ENV;
