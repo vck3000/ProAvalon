0cb5053ad66a86ae1f92a83686bec8f4111d9ea4
Kevin <kevinwong328@gmail.com>
2024-06-17 16:30:28 +1000

    Added required env variables for some general ones

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,13 +6,13 @@ import { VpnConfig, VpnConfigType } from './vpnConfig';
 
 type ConfigNew = {
   ENV: string;
-  NODE_ENV: string;
-  SERVER_DOMAIN: string;
-  PORT: string;
-  IP: string;
+  NODE_ENV?: string;
+  SERVER_DOMAIN?: string;
+  PORT?: string;
+  IP?: string;
   MY_SECRET_KEY: string;
 
-  GOOGLE_CAPTCHA_KEY: string;
+  GOOGLE_CAPTCHA_KEY?: string;
   DATABASE_URL: string;
 
   discord: DiscordConfigType;
@@ -28,10 +28,10 @@ export const config: Readonly<ConfigNew> = Object.freeze({
   SERVER_DOMAIN: process.env.SERVER_DOMAIN,
   PORT: process.env.PORT,
   IP: process.env.IP,
-  MY_SECRET_KEY: process.env.MY_SECRET_KEY,
+  MY_SECRET_KEY: getRequiredEnvVariable('MY_SECRET_KEY'),
 
   GOOGLE_CAPTCHA_KEY: process.env.MY_SECRET_GOOGLE_CAPTCHA_KEY,
-  DATABASE_URL: process.env.DATABASEURL, // TODO-kev: Consider renaming env variable
+  DATABASE_URL: getRequiredEnvVariable('DATABASEURL'), // TODO-kev: Consider renaming env variable
 
   discord: DiscordConfig,
   email: EmailConfig,
@@ -42,15 +42,26 @@ export const config: Readonly<ConfigNew> = Object.freeze({
 
 function validateEnv() {
   const VALID_ENVIRONMENTS: Set<string> = new Set(['local', 'staging', 'prod']);
+  const ENV = getRequiredEnvVariable('ENV');
 
   if (process.env.NODE_ENV !== 'test') {
-    if (!VALID_ENVIRONMENTS.has(process.env.ENV)) {
+    if (!VALID_ENVIRONMENTS.has(ENV)) {
       // TODO-kev: Prefer the console.error then process exit or throw an error?
-      console.error(`Bad environment variable given: ${process.env.ENV}`);
-      throw new Error(`Invalid settings: ENV=${process.env.ENV}`);
+      console.error(`Bad environment variable given: ${ENV}`);
+      throw new Error(`Invalid settings: ENV=${ENV}`);
       process.exit(1);
     }
   }
 
-  return process.env.ENV;
+  return ENV;
+}
+
+export function getRequiredEnvVariable(variableName: string) {
+  const envVariable = process.env[variableName];
+
+  if (envVariable === undefined || envVariable === '') {
+    throw new Error(`Missing required environment variable: ${variableName}`);
+  }
+
+  return envVariable;
 }
