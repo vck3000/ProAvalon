f75553a4d7e1921f9abbdf9d1633d52796207ede
Kevin <kevinwong328@gmail.com>
2024-07-06 20:13:00 +1000

    Fixed inc mock

--- src/clients/victoriaMetrics/tests/promMetricCounter.test.ts
+++ src/clients/victoriaMetrics/tests/promMetricCounter.test.ts
@@ -1,3 +1,4 @@
+import { Counter } from 'prom-client';
 import { promAgent } from '../promAgent';
 import {
   CounterConfig,
@@ -8,13 +9,13 @@ import {
 // Create mocks for promAgent and promClient.Counter
 promAgent.registerMetric = jest.fn();
 
+const incMock = jest.fn();
+
 jest.mock('prom-client', () => ({
   Counter: jest.fn().mockImplementation(() => ({
-    inc: jest.fn(), // Mock the inc method
+    inc: incMock, // Mock the inc method
   })),
 }));
-
-import { Counter } from 'prom-client';
 
 describe('PromMetricCounter', () => {
   describe('Constructor', () => {
@@ -75,41 +76,37 @@ describe('PromMetricCounter', () => {
       expect(() => new PromMetricCounter(counterConfig)).toThrow();
     });
 
-    // it('initialise all metric combinations where labelOptions are present.', () => {
-    //   new PromMetricCounter({
-    //     name: 'test_counter',
-    //     help: 'A test counter.',
-    //     labelOptions: {
-    //       status: new Set(['finished', 'voided']),
-    //       colour: new Set(['black', 'white']),
-    //     },
-    //   });
-    //
-    //   const expectedConfig: PromClientCounterConfig = {
-    //     name: 'test_counter',
-    //     help: 'A test counter.',
-    //     labelNames: ['status', 'colour'],
-    //   };
-    //
-    //   expect(promAgent.registerMetric).toHaveBeenCalledWith('test_counter');
-    //   expect(Counter).toHaveBeenCalledWith(expectedConfig);
-    //
-    //   const labelCombinations = [
-    //     { status: 'finished', colour: 'black' },
-    //     { status: 'finished', colour: 'white' },
-    //     { status: 'voided', colour: 'black' },
-    //     { status: 'voided', colour: 'white' },
-    //   ];
-    //
-    //   console.log('test');
-    //   console.log(Counter.inc);
-    //
-    //   // labelCombinations.forEach((combination) => {
-    //   //   expect(MockCounter.mock.instances[0].inc).toHaveBeenCalledWith(
-    //   //     combination,
-    //   //     0,
-    //   //   );
-    //   // });
-    // });
+    it('initialise all metric combinations where labelOptions are present.', () => {
+      new PromMetricCounter({
+        name: 'test_counter',
+        help: 'A test counter.',
+        labelOptions: {
+          status: new Set(['finished', 'voided']),
+          colour: new Set(['black', 'white']),
+        },
+      });
+
+      const expectedConfig: PromClientCounterConfig = {
+        name: 'test_counter',
+        help: 'A test counter.',
+        labelNames: ['status', 'colour'],
+      };
+
+      expect(promAgent.registerMetric).toHaveBeenCalledWith('test_counter');
+      expect(Counter).toHaveBeenCalledWith(expectedConfig);
+
+      const labelCombinations = [
+        { status: 'finished', colour: 'black' },
+        { status: 'finished', colour: 'white' },
+        { status: 'voided', colour: 'black' },
+        { status: 'voided', colour: 'white' },
+      ];
+
+      expect(incMock).toHaveBeenCalledTimes(4);
+
+      labelCombinations.forEach((combination) => {
+        expect(incMock).toHaveBeenCalledWith(combination, 0);
+      });
+    });
   });
 });
