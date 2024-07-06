d0dae22f8fbcae6fda82bec48caf1cad0e5acc85
Kevin <kevinwong328@gmail.com>
2024-07-06 22:03:07 +1000

    Changed from recursive back to map

--- src/clients/victoriaMetrics/metricFunctions.ts
+++ src/clients/victoriaMetrics/metricFunctions.ts
@@ -1,43 +1,19 @@
 export function generateLabelCombinations(
   labelOptions: Record<string, Set<string>>,
 ): Record<string, string>[] {
-  const labelNames = Object.keys(labelOptions);
-  const labelCombinations: Record<string, string>[] = [];
+  let labelCombinations: Record<string, string>[] = [{}];
 
-  function buildCombinations(
-    currentCombination: Record<string, string>,
-    index: number,
-  ) {
-    if (index === labelNames.length) {
-      labelCombinations.push(currentCombination);
-      return;
-    }
+  Object.keys(labelOptions).forEach((labelName) => {
+    // Get current label options
+    const options = Array.from(labelOptions[labelName]);
 
-    const key = labelNames[index];
-    const options = Array.from(labelOptions[key]);
-
-    options.forEach((option) => {
-      buildCombinations({ ...currentCombination, [key]: option }, index + 1);
-    });
-  }
-
-  buildCombinations({}, 0);
+    labelCombinations = labelCombinations.flatMap((combination) =>
+      options.map((option) => ({
+        ...combination,
+        [labelName]: option,
+      })),
+    );
+  });
 
   return labelCombinations;
-
-  // TODO-kev: Use above recursive solution or below compact one
-  // Object.keys(labelOptions).forEach((labelName) => {
-  //   // Get current label options
-  //   const options = Array.from(labelOptions[labelName]);
-  //
-  //   // Update combinations with new options
-  //   labelCombinations = labelCombinations.flatMap((combination) =>
-  //     options.map((option) => ({
-  //       ...combination,
-  //       [labelName]: option,
-  //     })),
-  //   );
-  // });
-  //
-  // return labelCombinations;
 }
