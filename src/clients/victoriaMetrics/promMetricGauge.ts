fea929b6dccd1fcc1b5af610f81804e7cb19c70b
Kevin <kevinwong328@gmail.com>
2024-07-06 22:54:03 +1000

    Updated gauge

--- src/clients/victoriaMetrics/promMetricGauge.ts
+++ src/clients/victoriaMetrics/promMetricGauge.ts
@@ -1,43 +1,55 @@
 import promClient, { Gauge } from 'prom-client';
 import { promAgent } from './promAgent';
-import { generateLabelCombinations } from './metricFunctions';
 
 interface GaugeConfig {
   name: string;
   help: string;
-  labelNames?: string[];
-  labelOptions?: Record<string, string[]>;
+  labelOptions?: Record<string, Set<string>>;
   collect?: () => void; // Refer to prom-client docs on how this should be used.
 }
 
+interface PromClientGaugeConfig {
+  name: string;
+  help: string;
+  labelNames?: string[];
+  collect?: () => void;
+}
+
 export class PromMetricGauge {
-  private readonly labelCombinations: Record<string, string>[];
+  private readonly labelOptions: Record<string, Set<string>>;
   private gauge: Gauge;
 
   constructor(gaugeConfig: GaugeConfig) {
     promAgent.registerMetric(gaugeConfig.name);
 
-    // Check either both or neither labelNames and labelOptions are declared
-    if (
-      (gaugeConfig.labelNames && !gaugeConfig.labelOptions) ||
-      (!gaugeConfig.labelNames && gaugeConfig.labelOptions)
-    ) {
-      throw new Error(
-        `Error: Counter Metric "${gaugeConfig.name}" must have both labelNames and labelOptions configured if either is declared.`,
-      );
-    }
+    if (!gaugeConfig.labelOptions) {
+      this.gauge = new promClient.Gauge({
+        name: gaugeConfig.name,
+        help: gaugeConfig.help,
+        collect: gaugeConfig.collect,
+      });
+    } else {
+      if (Object.keys(gaugeConfig.labelOptions).length === 0) {
+        throw new Error('LabelOptions are declared but undefined.');
+      }
 
-    // Validate labelNames and labelOptions if both provided
-    // TODO-kev: Needs to be fixed
-    // if (gaugeConfig.labelNames && gaugeConfig.labelOptions) {
-    //   validateLabelNamesAndOptions(
-    //     gaugeConfig.labelNames,
-    //     gaugeConfig.labelOptions,
-    //   );
-    //   this.labelCombinations = generateLabelCombinations(
-    //     gaugeConfig.labelOptions,
-    //   );
-    // }
+      for (const labelName in gaugeConfig.labelOptions) {
+        if (gaugeConfig.labelOptions[labelName].size === 0) {
+          throw new Error(
+            `LabelOptions are undefined for labelName: "${labelName}".`,
+          );
+        }
+      }
+
+      // Initialise gauge metric
+      this.labelOptions = gaugeConfig.labelOptions;
+      this.gauge = new promClient.Gauge({
+        name: gaugeConfig.name,
+        help: gaugeConfig.help,
+        labelNames: Object.keys(gaugeConfig.labelOptions),
+        collect: gaugeConfig.collect,
+      });
+    }
 
     this.gauge = new promClient.Gauge(gaugeConfig);
   }
