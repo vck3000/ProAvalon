e8095cc6816aae1694a76b42193daf0b9a4238ae
Kevin <kevinwong328@gmail.com>
2024-08-27 09:56:06 +1000

    Updated tourney users + announcement message

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,6 +1,6 @@
 // all in lower case
 // TO = Tournament Organizer
-export const TOsArray: string[] = ['anji'];
+export const TOsArray: string[] = ['Tree3', 'Ref-Rain'];
 
 export function isTO(username: string): boolean {
   return TOsArray.includes(username.toLowerCase());
