c6b75f7d17c1f3a0c5dc79e579721e25c8f63bf5
Kevin <kevinwong328@gmail.com>
2024-08-26 23:57:23 +1000

    Updated tourney organisers partially

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,14 +1,6 @@
 // all in lower case
 // TO = Tournament Organizer
-export const TOsArray: string[] = [
-  'pat',
-  'pandamania',
-  'glorious',
-  'manuavalon',
-  'bremen',
-  'starkrush',
-  'secretwoa',
-];
+export const TOsArray: string[] = ['anji'];
 
 export function isTO(username: string): boolean {
   return TOsArray.includes(username.toLowerCase());
