a1d2ad4d4c4d1cdbef486cccfce25236d8809501
Kevin <kevinwong328@gmail.com>
2024-09-16 20:20:09 +1000

    Cleared tourney organisers and removed tourney message

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,6 +1,6 @@
 // all in lower case
 // TO = Tournament Organizer
-export const TOsArray: string[] = ['Tree3', 'Ref-Rain'];
+export const TOsArray: string[] = [];
 
 export function isTO(username: string): boolean {
   return TOsArray.includes(username.toLowerCase());
