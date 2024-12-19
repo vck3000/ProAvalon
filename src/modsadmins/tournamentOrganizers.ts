a356c04820de0f43015fecea5f4525aaec19490b
Kevin <118262634+kev306@users.noreply.github.com>
2024-12-19 11:09:20 +1100

    Reset tourney organisers (#725)

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,12 +1,6 @@
 // all in lower case
 // TO = Tournament Organizer
-export const TOsArray: string[] = [
-  'pandamania',
-  'glorious',
-  'manuavalon',
-  'datboi',
-  'secretwoa',
-];
+export const TOsArray: string[] = [];
 
 export function isTO(username: string): boolean {
   return TOsArray.includes(username.toLowerCase());
