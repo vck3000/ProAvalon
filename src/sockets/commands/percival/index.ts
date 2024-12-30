80faaed121c69172b5596fd6e316e5efb76278a0
Kevin <kevinwong328@gmail.com>
2024-12-30 14:58:38 +1100

    Added percival helper command

--- src/sockets/commands/percival/index.ts
+++ src/sockets/commands/percival/index.ts
@@ -1,9 +1,11 @@
 import { Commands } from '../types';
-import { mdc } from '../mod/mdc';
 import { adjustCommandPrefix } from '../helperFunctions';
+import { p } from './p';
+import { mdc } from '../mod/mdc';
 
 const pdc = adjustCommandPrefix(mdc, 'm', 'p');
 
 export const percivalCommands: Commands = {
+  [p.command]: p,
   [pdc.command]: pdc,
 };
