9b308ed0bb79d147752df68c69b9dfe466c075f3
Kevin <kevinwong328@gmail.com>
2025-01-14 21:23:50 +1100

    Added resetSeason function to seasonAdapter

--- src/databaseAdapters/season.ts
+++ src/databaseAdapters/season.ts
@@ -4,11 +4,12 @@ import { ISeason } from '../models/types/season.types';
 interface DatabaseAdapter {
   getCurrentSeason(): Promise<ISeason | null>;
   createSeason(seasonName: string): Promise<ISeason>;
+  resetSeason(newSeasonName: string): Promise<void>;
 }
 
 class MongoSeasonAdapter implements DatabaseAdapter {
   async getCurrentSeason(): Promise<ISeason | null> {
-    const currentSeason = await Season.findOne({
+    const currentSeason: ISeason | null = await Season.findOne({
       isActive: true,
     });
 
@@ -39,6 +40,29 @@ class MongoSeasonAdapter implements DatabaseAdapter {
     );
 
     return newSeason as ISeason;
+  }
+
+  async resetSeason(newSeasonName: string): Promise<void> {
+    const currentSeason: ISeason | null = await this.getCurrentSeason();
+
+    if (currentSeason) {
+      // Check if ongoing season
+      if (currentSeason.endDate > new Date()) {
+        throw new Error(
+          `Unable to reset season while season is ongoing. Season ends on ${currentSeason.endDate}.`,
+        );
+      }
+
+      // Deactivate the current season
+      currentSeason.isActive = false;
+      await currentSeason.save();
+      console.log(
+        `Deactivated season: id=${currentSeason._id}; name=${currentSeason.name}`,
+      );
+    }
+
+    // Create a new season
+    await this.createSeason(newSeasonName);
   }
 }
 
