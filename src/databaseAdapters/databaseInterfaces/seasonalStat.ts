51b64a192a4542e032d3dfd2b224a863ecde2464
Kevin <kevinwong328@gmail.com>
2025-01-23 22:54:14 +1100

    Updated ISeasonalStatDbAdapter

--- src/databaseAdapters/databaseInterfaces/seasonalStat.ts
+++ src/databaseAdapters/databaseInterfaces/seasonalStat.ts
@@ -3,6 +3,10 @@ import { ISeasonalStat } from '../../models/types/seasonalStats';
 
 export default interface ISeasonalStatDbAdapter {
   parseSeasonalStat(stat: ISeasonalStat): string;
+  createStat(
+    userId: Types.ObjectId,
+    seasonId: Types.ObjectId,
+  ): Promise<ISeasonalStat>;
   getStat(
     userId: Types.ObjectId,
     seasonId: Types.ObjectId,
