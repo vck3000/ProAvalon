5f32a0d9c129eacd3adf1631bef5dcc113e5ed45
Kevin <kevinwong328@gmail.com>
2025-01-25 17:24:52 +1100

    Fixed acreateseason

--- src/sockets/commands/admin/acreateseason.ts
+++ src/sockets/commands/admin/acreateseason.ts
@@ -5,6 +5,7 @@ import { SocketUser } from '../../types';
 import mongoDbAdapter from '../../../databaseAdapters/mongoose';
 import { sendReplyToCommand } from '../../sockets';
 import { ISeason } from '../../../models/types/season';
+import { RatingBracket } from '../../../databaseAdapters/mongoose/season';
 
 export const acreateseason: Command = {
   command: 'acs',
@@ -16,10 +17,27 @@ export const acreateseason: Command = {
     }
 
     const seasonName = args[1];
+    const startDate = new Date();
+    const endDate = new Date(startDate);
+    endDate.setMonth(startDate.getMonth() + 3); // 3 months
+
+    // TODO-kev: Consider where to place this
+    const ratingBrackets: RatingBracket[] = [
+      { name: 'iron', min: 0, max: 1299 }, // Lower limit set at 0
+      { name: 'bronze', min: 1300, max: 1399 },
+      { name: 'silver', min: 1400, max: 1549 },
+      { name: 'gold', min: 1550, max: 1699 },
+      { name: 'platinum', min: 1700, max: 1799 },
+      { name: 'diamond', min: 1800, max: 1899 },
+      { name: 'champion', min: 1900, max: Infinity }, // Must have no upper limit
+    ];
 
     try {
       const newSeason: ISeason = await mongoDbAdapter.season.createSeason(
         seasonName,
+        startDate,
+        endDate,
+        ratingBrackets,
       );
       const message = `Created new season: ${mongoDbAdapter.season.parseSeason(
         newSeason,
