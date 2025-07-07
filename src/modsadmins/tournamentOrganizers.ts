1dd5087ad20dc244640be34318f43ad95ff56e0f
Victor Kuo <vck3000@gmail.com>
2025-07-08 09:06:50 +1000

    Remove TOs

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,10 +1,6 @@
 // all in lower case
 // TO = Tournament Organizer
 export const TOsArray: string[] = [
-  'pandamania',
-  'glorious',
-  'manuavalon',
-  'feelsriddle',
 ];
 
 export function isTO(username: string): boolean {
