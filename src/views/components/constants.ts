68025a986347f8eb1499520da409602fd3bacc2e
QuinnWuest <qwuest123@gmail.com>
2025-11-13 04:23:13 -0500

    Use 1024x1024 PNG avatars instead of SVG avatars

--- src/views/components/constants.ts
+++ src/views/components/constants.ts
@@ -1,4 +1,4 @@
 export const BaseAvatarLinks = {
-  baseRes: '/avatars/base-res.svg',
-  baseSpy: '/avatars/base-spy.svg',
+  baseRes: '/avatars/base-res-1024x1024.png',
+  baseSpy: '/avatars/base-spy-1024x1024.png',
 };
