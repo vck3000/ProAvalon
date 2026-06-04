f06d0b232b579157342d1ef1ddc57cb43dbe4634
SadNixon <49939932+sadnixon@users.noreply.github.com>
2026-06-04 08:22:11 -0700

    Update tournamentOrganizers.ts (#784)

--- src/modsadmins/tournamentOrganizers.ts
+++ src/modsadmins/tournamentOrganizers.ts
@@ -1,6 +1,14 @@
 // all in lower case
 // TO = Tournament Organizer
-export const TOsArray: string[] = [];
+export const TOsArray: string[] = [
+  'ingenue',
+  'glorious',
+  'mirroringhim',
+  'feelsriddle',
+  'eevee',
+  'bellzzzz',
+  'datboi',
+];
 
 export function isTO(username: string): boolean {
   return TOsArray.includes(username.toLowerCase());
